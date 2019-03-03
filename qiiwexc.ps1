f0dad6819dd2ca26f6b79981823e3e761509369f
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2019-03-04 00:39:40 +0200

    Change download endpoints

--- qiiwexc.ps1
+++ qiiwexc.ps1
@@ -612,3 +612,18 @@ function DownloadFile ($URL, $FileName) {
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-# Draw Form #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
 
 $_FORM.ShowDialog() | Out-Null
+
+# powershell.exe -Version 2
+# $psversiontable
+
+# Clear-Host
+# Write-Host -NoNewLine 'Press any key to continue...';
+# $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+
+# Optimize `GatherSystemInformation`
+
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
+
+# New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main' -Name 'DisableFirstRunCustomize' -Value 2 -PropertyType DWORD -Force | Out-Null
+
+# $_TAB_CONTROL.SelectedTab = $_TAB_DOWNLOADS
