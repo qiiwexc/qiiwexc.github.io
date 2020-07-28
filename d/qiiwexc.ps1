06fe2a183fb72000dfe0940c70f2f44b5a195808
qiiwexc <romans.ivanovs.x@gmail.com>
2020-07-28 14:41:44 +0300

    Update Rufus 3.10 -> 3.11, add Windows PE based on Windows 10

--- d/qiiwexc.ps1
+++ d/qiiwexc.ps1
@@ -1,4 +1,4 @@
-Set-Variable -Option Constant Version ([Version]'20.5.26')
+Set-Variable -Option Constant Version ([Version]'20.7.28')
 
 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-# Info #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
@@ -276,38 +276,32 @@ $CBOX_StartChewWGA.Add_CheckStateChanged( { $BTN_DownloadChewWGA.Text = "ChewWGA
 
 Set-Variable -Option Constant GRP_DownloadTools (New-Object System.Windows.Forms.GroupBox)
 $GRP_DownloadTools.Text = 'Tools (General)'
-$GRP_DownloadTools.Height = $INT_GROUP_TOP + $INT_BTN_LONG * 3
+$GRP_DownloadTools.Height = $INT_GROUP_TOP + $INT_BTN_LONG * 2
 $GRP_DownloadTools.Width = $GRP_WIDTH
 $GRP_DownloadTools.Location = $GRP_Activators.Location + $SHIFT_GRP_HOR_NORMAL
 $TAB_HOME.Controls.Add($GRP_DownloadTools)
 
 Set-Variable -Option Constant BTN_DownloadRufus (New-Object System.Windows.Forms.Button)
-Set-Variable -Option Constant BTN_DownloadDSE   (New-Object System.Windows.Forms.Button)
 Set-Variable -Option Constant CBOX_StartRufus   (New-Object System.Windows.Forms.CheckBox)
-Set-Variable -Option Constant CBOX_StartDSE     (New-Object System.Windows.Forms.CheckBox)
 
 Set-Variable -Option Constant BTN_WindowsPE     (New-Object System.Windows.Forms.Button)
 Set-Variable -Option Constant LBL_WindowsPE     (New-Object System.Windows.Forms.Label)
 
 (New-Object System.Windows.Forms.ToolTip).SetToolTip($BTN_DownloadRufus, 'Download Rufus - a bootable USB creator')
-(New-Object System.Windows.Forms.ToolTip).SetToolTip($BTN_DownloadDSE, 'Download Driver Store Explorer')
-(New-Object System.Windows.Forms.ToolTip).SetToolTip($BTN_WindowsPE, 'Download Windows PE (Live CD) ISO image based on Windows 8')
-
+(New-Object System.Windows.Forms.ToolTip).SetToolTip($BTN_WindowsPE, 'Download Windows PE (Live CD) ISO image based on Windows 10')
 (New-Object System.Windows.Forms.ToolTip).SetToolTip($CBOX_StartRufus, $TIP_START_AFTER_DOWNLOAD)
-(New-Object System.Windows.Forms.ToolTip).SetToolTip($CBOX_StartDSE, $TIP_START_AFTER_DOWNLOAD)
 
-$BTN_DownloadRufus.Font = $BTN_DownloadDSE.Font = $BTN_WindowsPE.Font = $BTN_FONT
-$BTN_DownloadRufus.Height = $BTN_DownloadDSE.Height = $BTN_WindowsPE.Height = $BTN_HEIGHT
-$BTN_DownloadRufus.Width = $BTN_DownloadDSE.Width = $BTN_WindowsPE.Width = $BTN_WIDTH
+$BTN_DownloadRufus.Font = $BTN_WindowsPE.Font = $BTN_FONT
+$BTN_DownloadRufus.Height = $BTN_WindowsPE.Height = $BTN_HEIGHT
+$BTN_DownloadRufus.Width = $BTN_WindowsPE.Width = $BTN_WIDTH
+$CBOX_StartRufus.Size = $LBL_WindowsPE.Size = $CBOX_SIZE
 
-$CBOX_StartRufus.Size = $CBOX_StartDSE.Size = $LBL_WindowsPE.Size = $CBOX_SIZE
-
-$GRP_DownloadTools.Controls.AddRange(@($BTN_DownloadRufus, $CBOX_StartRufus, $BTN_DownloadDSE, $CBOX_StartDSE, $BTN_WindowsPE, $LBL_WindowsPE))
+$GRP_DownloadTools.Controls.AddRange(@($BTN_DownloadRufus, $CBOX_StartRufus, $BTN_WindowsPE, $LBL_WindowsPE))
 
 
 $BTN_DownloadRufus.Text = "Rufus (bootable USB)$REQUIRES_ELEVATION"
 $BTN_DownloadRufus.Location = $BTN_INIT_LOCATION
-$BTN_DownloadRufus.Add_Click( { Start-DownloadExtractExecute -Execute:$CBOX_StartRufus.Checked 'github.com/pbatard/rufus/releases/download/v3.10/rufus-3.10p.exe' -Params:'-g' } )
+$BTN_DownloadRufus.Add_Click( { Start-DownloadExtractExecute -Execute:$CBOX_StartRufus.Checked 'github.com/pbatard/rufus/releases/download/v3.11/rufus-3.11p.exe' -Params:'-g' } )
 
 $CBOX_StartRufus.Location = $BTN_DownloadRufus.Location + $SHIFT_CBOX_EXECUTE
 $CBOX_StartRufus.Text = $TXT_START_AFTER_DOWNLOAD
@@ -315,19 +309,9 @@ $CBOX_StartRufus.Checked = $True
 $CBOX_StartRufus.Add_CheckStateChanged( { $BTN_DownloadRufus.Text = "Rufus (bootable USB)$(if ($CBOX_StartRufus.Checked) {$REQUIRES_ELEVATION})" } )
 
 
-$BTN_DownloadDSE.Text = "Driver Store Explorer$REQUIRES_ELEVATION"
-$BTN_DownloadDSE.Location = $BTN_DownloadRufus.Location + $SHIFT_BTN_LONG
-$BTN_DownloadDSE.Add_Click( { Start-DownloadExtractExecute -Execute:$CBOX_StartDSE.Checked 'github.com/lostindark/DriverStoreExplorer/releases/download/v0.10.58/DriverStoreExplorer.v0.10.58.zip' } )
-
-$CBOX_StartDSE.Location = $BTN_DownloadDSE.Location + $SHIFT_CBOX_EXECUTE
-$CBOX_StartDSE.Text = $TXT_START_AFTER_DOWNLOAD
-$CBOX_StartDSE.Checked = $True
-$CBOX_StartDSE.Add_CheckStateChanged( { $BTN_DownloadDSE.Text = "Driver Store Explorer$(if ($CBOX_StartDSE.Checked) {$REQUIRES_ELEVATION})" } )
-
-
 $BTN_WindowsPE.Text = 'Windows PE (Live CD)'
-$BTN_WindowsPE.Location = $BTN_DownloadDSE.Location + $SHIFT_BTN_LONG
-$BTN_WindowsPE.Add_Click( { Open-InBrowser 'drive.google.com/uc?id=1IYwATgzmKmlc79lVi0ivmWM2aPJObmq_' } )
+$BTN_WindowsPE.Location = $BTN_DownloadRufus.Location + $SHIFT_BTN_LONG
+$BTN_WindowsPE.Add_Click( { Open-InBrowser 'drive.google.com/file/d/1HfjCrylPOOmwQf31yTl94VCSi4quoliF' } )
 
 $LBL_WindowsPE.Text = $TXT_OPENS_IN_BROWSER
 $LBL_WindowsPE.Location = $BTN_WindowsPE.Location + $SHIFT_LBL_BROWSER
@@ -628,7 +612,7 @@ $LBL_Windows10.Location = $BTN_Windows10.Location + $SHIFT_LBL_BROWSER
 
 $BTN_Windows8.Text = 'Windows 8.1 (Update 3)'
 $BTN_Windows8.Location = $BTN_Windows10.Location + $SHIFT_BTN_LONG
-$BTN_Windows8.Add_Click( { Open-InBrowser 'http://w13.monkrus.ws/2020/03/windows-81-rus-eng-x86-x64-20in1.html' } )
+$BTN_Windows8.Add_Click( { Open-InBrowser 'http://monkrus.ws/2020/03/windows-81-rus-eng-x86-x64-20in1.html' } )
 
 $LBL_Windows8.Location = $BTN_Windows8.Location + $SHIFT_LBL_BROWSER
 
@@ -641,13 +625,13 @@ $LBL_Windows7.Location = $BTN_Windows7.Location + $SHIFT_LBL_BROWSER
 
 $BTN_WindowsXPENG.Text = 'Windows XP SP3 (ENG)'
 $BTN_WindowsXPENG.Location = $BTN_Windows7.Location + $SHIFT_BTN_NORMAL + $SHIFT_CBOX_SHORT
-$BTN_WindowsXPENG.Add_Click( { Open-InBrowser 'drive.google.com/uc?id=1TO6cR3QiicCcAxcRba65L7nMvWTaFQaF' } )
+$BTN_WindowsXPENG.Add_Click( { Open-InBrowser 'drive.google.com/file/d/1TO6cR3QiicCcAxcRba65L7nMvWTaFQaF' } )
 
 $LBL_WindowsXPENG.Location = $BTN_WindowsXPENG.Location + $SHIFT_LBL_BROWSER
 
 $BTN_WindowsXPRUS.Text = 'Windows XP SP3 (RUS)'
 $BTN_WindowsXPRUS.Location = $BTN_WindowsXPENG.Location + $SHIFT_BTN_LONG
-$BTN_WindowsXPRUS.Add_Click( { Open-InBrowser 'drive.google.com/uc?id=1mgs56mX2-dQMk9e5KaXhODLBWXipmLCR' } )
+$BTN_WindowsXPRUS.Add_Click( { Open-InBrowser 'drive.google.com/file/d/1mgs56mX2-dQMk9e5KaXhODLBWXipmLCR' } )
 
 $LBL_WindowsXPRUS.Location = $BTN_WindowsXPRUS.Location + $SHIFT_LBL_BROWSER
 
@@ -2142,7 +2126,6 @@ Function Start-FileCleanup {
         "$env:LocalAppData\Microsoft\Windows\SettingSync\metastore\*.log"
         "$env:LocalAppData\Microsoft\Windows\SettingSync\remotemetastore\v1\*.log"
         "$env:LocalAppData\Microsoft\Windows\WebCache\*.log"
-        "$env:LocalAppData\PeerDistRepub"
         "$env:LocalAppData\Razer\Synapse3\Log"
         "$env:LocalAppData\Razer\Synapse3\Log\*"
         "$env:LocalAppData\VirtualStore"
--- src/1 GUI/13 Home - Tools (General).ps1
+++ src/1 GUI/13 Home - Tools (General).ps1
@@ -1,37 +1,31 @@
 Set-Variable -Option Constant GRP_DownloadTools (New-Object System.Windows.Forms.GroupBox)
 $GRP_DownloadTools.Text = 'Tools (General)'
-$GRP_DownloadTools.Height = $INT_GROUP_TOP + $INT_BTN_LONG * 3
+$GRP_DownloadTools.Height = $INT_GROUP_TOP + $INT_BTN_LONG * 2
 $GRP_DownloadTools.Width = $GRP_WIDTH
 $GRP_DownloadTools.Location = $GRP_Activators.Location + $SHIFT_GRP_HOR_NORMAL
 $TAB_HOME.Controls.Add($GRP_DownloadTools)
 
 Set-Variable -Option Constant BTN_DownloadRufus (New-Object System.Windows.Forms.Button)
-Set-Variable -Option Constant BTN_DownloadDSE   (New-Object System.Windows.Forms.Button)
 Set-Variable -Option Constant CBOX_StartRufus   (New-Object System.Windows.Forms.CheckBox)
-Set-Variable -Option Constant CBOX_StartDSE     (New-Object System.Windows.Forms.CheckBox)
 
 Set-Variable -Option Constant BTN_WindowsPE     (New-Object System.Windows.Forms.Button)
 Set-Variable -Option Constant LBL_WindowsPE     (New-Object System.Windows.Forms.Label)
 
 (New-Object System.Windows.Forms.ToolTip).SetToolTip($BTN_DownloadRufus, 'Download Rufus - a bootable USB creator')
-(New-Object System.Windows.Forms.ToolTip).SetToolTip($BTN_DownloadDSE, 'Download Driver Store Explorer')
-(New-Object System.Windows.Forms.ToolTip).SetToolTip($BTN_WindowsPE, 'Download Windows PE (Live CD) ISO image based on Windows 8')
-
+(New-Object System.Windows.Forms.ToolTip).SetToolTip($BTN_WindowsPE, 'Download Windows PE (Live CD) ISO image based on Windows 10')
 (New-Object System.Windows.Forms.ToolTip).SetToolTip($CBOX_StartRufus, $TIP_START_AFTER_DOWNLOAD)
-(New-Object System.Windows.Forms.ToolTip).SetToolTip($CBOX_StartDSE, $TIP_START_AFTER_DOWNLOAD)
 
-$BTN_DownloadRufus.Font = $BTN_DownloadDSE.Font = $BTN_WindowsPE.Font = $BTN_FONT
-$BTN_DownloadRufus.Height = $BTN_DownloadDSE.Height = $BTN_WindowsPE.Height = $BTN_HEIGHT
-$BTN_DownloadRufus.Width = $BTN_DownloadDSE.Width = $BTN_WindowsPE.Width = $BTN_WIDTH
+$BTN_DownloadRufus.Font = $BTN_WindowsPE.Font = $BTN_FONT
+$BTN_DownloadRufus.Height = $BTN_WindowsPE.Height = $BTN_HEIGHT
+$BTN_DownloadRufus.Width = $BTN_WindowsPE.Width = $BTN_WIDTH
+$CBOX_StartRufus.Size = $LBL_WindowsPE.Size = $CBOX_SIZE
 
-$CBOX_StartRufus.Size = $CBOX_StartDSE.Size = $LBL_WindowsPE.Size = $CBOX_SIZE
-
-$GRP_DownloadTools.Controls.AddRange(@($BTN_DownloadRufus, $CBOX_StartRufus, $BTN_DownloadDSE, $CBOX_StartDSE, $BTN_WindowsPE, $LBL_WindowsPE))
+$GRP_DownloadTools.Controls.AddRange(@($BTN_DownloadRufus, $CBOX_StartRufus, $BTN_WindowsPE, $LBL_WindowsPE))
 
 
 $BTN_DownloadRufus.Text = "Rufus (bootable USB)$REQUIRES_ELEVATION"
 $BTN_DownloadRufus.Location = $BTN_INIT_LOCATION
-$BTN_DownloadRufus.Add_Click( { Start-DownloadExtractExecute -Execute:$CBOX_StartRufus.Checked 'github.com/pbatard/rufus/releases/download/v3.10/rufus-3.10p.exe' -Params:'-g' } )
+$BTN_DownloadRufus.Add_Click( { Start-DownloadExtractExecute -Execute:$CBOX_StartRufus.Checked 'github.com/pbatard/rufus/releases/download/v3.11/rufus-3.11p.exe' -Params:'-g' } )
 
 $CBOX_StartRufus.Location = $BTN_DownloadRufus.Location + $SHIFT_CBOX_EXECUTE
 $CBOX_StartRufus.Text = $TXT_START_AFTER_DOWNLOAD
@@ -39,19 +33,9 @@ $CBOX_StartRufus.Checked = $True
 $CBOX_StartRufus.Add_CheckStateChanged( { $BTN_DownloadRufus.Text = "Rufus (bootable USB)$(if ($CBOX_StartRufus.Checked) {$REQUIRES_ELEVATION})" } )
 
 
-$BTN_DownloadDSE.Text = "Driver Store Explorer$REQUIRES_ELEVATION"
-$BTN_DownloadDSE.Location = $BTN_DownloadRufus.Location + $SHIFT_BTN_LONG
-$BTN_DownloadDSE.Add_Click( { Start-DownloadExtractExecute -Execute:$CBOX_StartDSE.Checked 'github.com/lostindark/DriverStoreExplorer/releases/download/v0.10.58/DriverStoreExplorer.v0.10.58.zip' } )
-
-$CBOX_StartDSE.Location = $BTN_DownloadDSE.Location + $SHIFT_CBOX_EXECUTE
-$CBOX_StartDSE.Text = $TXT_START_AFTER_DOWNLOAD
-$CBOX_StartDSE.Checked = $True
-$CBOX_StartDSE.Add_CheckStateChanged( { $BTN_DownloadDSE.Text = "Driver Store Explorer$(if ($CBOX_StartDSE.Checked) {$REQUIRES_ELEVATION})" } )
-
-
 $BTN_WindowsPE.Text = 'Windows PE (Live CD)'
-$BTN_WindowsPE.Location = $BTN_DownloadDSE.Location + $SHIFT_BTN_LONG
-$BTN_WindowsPE.Add_Click( { Open-InBrowser 'drive.google.com/uc?id=1IYwATgzmKmlc79lVi0ivmWM2aPJObmq_' } )
+$BTN_WindowsPE.Location = $BTN_DownloadRufus.Location + $SHIFT_BTN_LONG
+$BTN_WindowsPE.Add_Click( { Open-InBrowser 'drive.google.com/file/d/1HfjCrylPOOmwQf31yTl94VCSi4quoliF' } )
 
 $LBL_WindowsPE.Text = $TXT_OPENS_IN_BROWSER
 $LBL_WindowsPE.Location = $BTN_WindowsPE.Location + $SHIFT_LBL_BROWSER
