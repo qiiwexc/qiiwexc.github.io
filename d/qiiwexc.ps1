43a53e5170c8d5345904d144ca40ec3dd41b90be
qiiwexc <romans.ivanovs.x@gmail.com>
2021-03-28 01:04:38 +0200

    Update Victoria 5.35 -> 5.36, Office installer 7.1.4 -> 7.1.8

--- d/qiiwexc.ps1
+++ d/qiiwexc.ps1
@@ -1,4 +1,4 @@
-Set-Variable -Option Constant Version ([Version]'21.2.12')
+Set-Variable -Option Constant Version ([Version]'21.3.28')
 
 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-# Info #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
@@ -255,16 +255,13 @@ $CBOX_StartAAct.Add_CheckStateChanged( { $BTN_DownloadAAct.Text = "AAct (Win 7+,
 
 Set-Variable -Option Constant GRP_DownloadWindows (New-Object System.Windows.Forms.GroupBox)
 $GRP_DownloadWindows.Text = 'Windows Images'
-$GRP_DownloadWindows.Height = $INT_GROUP_TOP + $INT_BTN_LONG * 4
+$GRP_DownloadWindows.Height = $INT_GROUP_TOP + $INT_BTN_LONG * 3
 $GRP_DownloadWindows.Width = $GRP_WIDTH
 $GRP_DownloadWindows.Location = $GRP_Activators.Location + $SHIFT_GRP_HOR_NORMAL
 $TAB_HOME.Controls.Add($GRP_DownloadWindows)
 
 Set-Variable -Option Constant BTN_Windows10 (New-Object System.Windows.Forms.Button)
 Set-Variable -Option Constant LBL_Windows10 (New-Object System.Windows.Forms.Label)
-
-Set-Variable -Option Constant BTN_Windows8  (New-Object System.Windows.Forms.Button)
-Set-Variable -Option Constant LBL_Windows8  (New-Object System.Windows.Forms.Label)
 
 Set-Variable -Option Constant BTN_Windows7  (New-Object System.Windows.Forms.Button)
 Set-Variable -Option Constant LBL_Windows7  (New-Object System.Windows.Forms.Label)
@@ -273,18 +270,17 @@ Set-Variable -Option Constant BTN_WindowsXP (New-Object System.Windows.Forms.But
 Set-Variable -Option Constant LBL_WindowsXP (New-Object System.Windows.Forms.Label)
 
 (New-Object System.Windows.Forms.ToolTip).SetToolTip($BTN_Windows10, 'Download Windows 10 (v20H2) RUS-ENG x86-x64 -28in1- HWID-act (AIO) ISO image')
-(New-Object System.Windows.Forms.ToolTip).SetToolTip($BTN_Windows8, 'Download Windows 8.1 RUS-ENG x86-x64 -20in1- SevenMod v3 (AIO) ISO image')
 (New-Object System.Windows.Forms.ToolTip).SetToolTip($BTN_Windows7, 'Download Windows 7 SP1 RUS-ENG x86-x64 -18in1- (AIO) ISO image')
 (New-Object System.Windows.Forms.ToolTip).SetToolTip($BTN_WindowsXP, 'Download Windows XP SP3 (ENG) + Office 2010 SP2 (ENG) [v17.5.6] ISO image')
 
-$BTN_Windows10.Font = $BTN_Windows8.Font = $BTN_Windows7.Font = $BTN_WindowsXP.Font = $BTN_FONT
-$BTN_Windows10.Height = $BTN_Windows8.Height = $BTN_Windows7.Height = $BTN_WindowsXP.Height = $BTN_HEIGHT
-$BTN_Windows10.Width = $BTN_Windows8.Width = $BTN_Windows7.Width = $BTN_WindowsXP.Width = $BTN_WIDTH
+$BTN_Windows10.Font = $BTN_Windows7.Font = $BTN_WindowsXP.Font = $BTN_FONT
+$BTN_Windows10.Height = $BTN_Windows7.Height = $BTN_WindowsXP.Height = $BTN_HEIGHT
+$BTN_Windows10.Width = $BTN_Windows7.Width = $BTN_WindowsXP.Width = $BTN_WIDTH
 
-$LBL_Windows10.Size = $LBL_Windows8.Size = $LBL_Windows7.Size = $LBL_WindowsXP.Size = $CBOX_SIZE
-$LBL_Windows10.Text = $LBL_Windows8.Text = $LBL_Windows7.Text = $LBL_WindowsXP.Text = $TXT_OPENS_IN_BROWSER
+$LBL_Windows10.Size = $LBL_Windows7.Size = $LBL_WindowsXP.Size = $CBOX_SIZE
+$LBL_Windows10.Text = $LBL_Windows7.Text = $LBL_WindowsXP.Text = $TXT_OPENS_IN_BROWSER
 
-$GRP_DownloadWindows.Controls.AddRange(@($BTN_Windows10, $LBL_Windows10, $BTN_Windows8, $LBL_Windows8, $BTN_Windows7, $LBL_Windows7, $BTN_WindowsXP, $LBL_WindowsXP))
+$GRP_DownloadWindows.Controls.AddRange(@($BTN_Windows10, $LBL_Windows10, $BTN_Windows7, $LBL_Windows7, $BTN_WindowsXP, $LBL_WindowsXP))
 
 
 
@@ -294,14 +290,8 @@ $BTN_Windows10.Add_Click( { Open-InBrowser 'http://monkrus.ws/2020/11/windows-10
 
 $LBL_Windows10.Location = $BTN_Windows10.Location + $SHIFT_LBL_BROWSER
 
-$BTN_Windows8.Text = 'Windows 8.1 (Update 3)'
-$BTN_Windows8.Location = $BTN_Windows10.Location + $SHIFT_BTN_LONG
-$BTN_Windows8.Add_Click( { Open-InBrowser 'http://monkrus.ws/2020/03/windows-81-rus-eng-x86-x64-20in1.html' } )
-
-$LBL_Windows8.Location = $BTN_Windows8.Location + $SHIFT_LBL_BROWSER
-
 $BTN_Windows7.Text = 'Windows 7 SP1'
-$BTN_Windows7.Location = $BTN_Windows8.Location + $SHIFT_BTN_LONG
+$BTN_Windows7.Location = $BTN_Windows10.Location + $SHIFT_BTN_LONG
 $BTN_Windows7.Add_Click( { Open-InBrowser 'http://monkrus.ws/2020/02/windows-7-sp1-rus-eng-x86-x64-18in1.html' } )
 
 $LBL_Windows7.Location = $BTN_Windows7.Location + $SHIFT_LBL_BROWSER
@@ -673,7 +663,7 @@ $RBTN_FullDiskCheck.Location = $RBTN_QuickDiskCheck.Location + $SHIFT_RBTN_FULL_
 
 $BTN_DownloadVictoria.Text = "Victoria (HDD scan)$REQUIRES_ELEVATION"
 $BTN_DownloadVictoria.Location = $BTN_CheckDisk.Location + $SHIFT_BTN_LONG
-$BTN_DownloadVictoria.Add_Click( { Start-DownloadExtractExecute -Execute:$CBOX_StartVictoria.Checked 'hdd.by/Victoria/Victoria535.zip' } )
+$BTN_DownloadVictoria.Add_Click( { Start-DownloadExtractExecute -Execute:$CBOX_StartVictoria.Checked 'hdd.by/Victoria/Victoria536.zip' } )
 
 $CBOX_StartVictoria.Location = $BTN_DownloadVictoria.Location + $SHIFT_CBOX_EXECUTE
 $CBOX_StartVictoria.Add_CheckStateChanged( { $BTN_DownloadVictoria.Text = "Victoria (HDD scan)$(if ($CBOX_StartVictoria.Checked) {$REQUIRES_ELEVATION})" } )
@@ -1899,7 +1889,6 @@ Function Start-FileCleanup {
         "$env:LocalAppData\Microsoft\OneDrive\setup\*"
         "$env:LocalAppData\Microsoft\Teams\*.log"
         "$env:LocalAppData\Microsoft\Teams\*.log"
-        "$env:LocalAppData\Microsoft\Teams\current\resources\ThirdPartyNotices.txt"
         "$env:LocalAppData\Microsoft\Teams\packages\*.nupkg"
         "$env:LocalAppData\Microsoft\Teams\packages\SquirrelTemp"
         "$env:LocalAppData\Microsoft\Teams\packages\SquirrelTemp\*"
