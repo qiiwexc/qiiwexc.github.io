1af3434e9dd1ad847144511f58b171adfea07b6e
qiiwexc <romans.ivanovs.x@gmail.com>
2022-04-28 02:43:44 +0300

    Merge MRT scan into the security scan

--- src/1 GUI/32 Maintenance - Software Diagnostics.ps1
+++ src/1 GUI/32 Maintenance - Software Diagnostics.ps1
@@ -7,12 +7,5 @@ New-Button -UAC 'Check Windows health' $BUTTON_FUNCTION -ToolTip $BUTTON_TOOLTIP
 
 
 $BUTTON_TOOLTIP_TEXT = 'Start security scan'
-$BUTTON_DISABLED = !(Test-Path $PATH_DEFENDER_EXE)
 $BUTTON_FUNCTION = { Start-SecurityScan }
-New-Button 'Perform a security scan' $BUTTON_FUNCTION -ToolTip $BUTTON_TOOLTIP_TEXT -Disabled:$BUTTON_DISABLED > $Null
-
-
-$BUTTON_TOOLTIP_TEXT = 'Start malware scan'
-$BUTTON_DISABLED = !(Test-Path $PATH_DEFENDER_EXE)
-$BUTTON_FUNCTION = { Start-MalwareScan }
-New-Button -UAC 'Perform a malware scan' $BUTTON_FUNCTION -ToolTip $BUTTON_TOOLTIP_TEXT -Disabled:$BUTTON_DISABLED > $Null
+New-Button 'Perform security scans' $BUTTON_FUNCTION -ToolTip $BUTTON_TOOLTIP_TEXT > $Null
