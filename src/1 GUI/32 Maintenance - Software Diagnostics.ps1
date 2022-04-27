c17ab1a1c01efba1ffac5b078ae73ba091b7c8dc
qiiwexc <romans.ivanovs.x@gmail.com>
2022-04-27 03:09:12 +0300

    Merge SFC into the Windows integrity checks step

--- src/1 GUI/32 Maintenance - Software Diagnostics.ps1
+++ src/1 GUI/32 Maintenance - Software Diagnostics.ps1
@@ -6,11 +6,6 @@ $BUTTON_FUNCTION = { Test-WindowsHealth }
 New-Button -UAC 'Check Windows health' $BUTTON_FUNCTION -ToolTip $BUTTON_TOOLTIP_TEXT > $Null
 
 
-$BUTTON_TOOLTIP_TEXT = 'Check system file integrity'
-$BUTTON_FUNCTION = { Repair-SystemFiles }
-New-Button -UAC 'Check system files' $BUTTON_FUNCTION -ToolTip $BUTTON_TOOLTIP_TEXT > $Null
-
-
 $BUTTON_TOOLTIP_TEXT = 'Start security scan'
 $BUTTON_DISABLED = !(Test-Path $PATH_DEFENDER_EXE)
 $BUTTON_FUNCTION = { Start-SecurityScan }
