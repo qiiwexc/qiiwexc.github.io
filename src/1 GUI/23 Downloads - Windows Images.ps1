527a4e381661346c76f03a353b49ad10da23306c
qiiwexc <romans.ivanovs.x@gmail.com>
2022-10-22 15:29:35 +0300

    Add Ventoy

--- src/1 GUI/23 Downloads - Windows Images.ps1
+++ src/1 GUI/23 Downloads - Windows Images.ps1
@@ -9,3 +9,6 @@ New-ButtonBrowser 'Windows 10 (v21H2)' $BUTTON_FUNCTION
 
 $BUTTON_FUNCTION = { Open-InBrowser $URL_WINDOWS_7 }
 New-ButtonBrowser 'Windows 7 SP1' $BUTTON_FUNCTION
+
+$BUTTON_FUNCTION = { Open-InBrowser 'https://rutracker.org/forum/viewtopic.php?t=4366725' }
+New-ButtonBrowser 'Windows PE (Live CD)' $BUTTON_FUNCTION
