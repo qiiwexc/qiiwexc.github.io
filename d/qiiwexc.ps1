4ef9899b11908f39afaa48fe8b9c72568b2855db
qiiwexc <romans.ivanovs.x@gmail.com>
2022-11-22 21:57:27 +0200

    Update Ventoy 1.0.81 -> 1.0.82

--- d/qiiwexc.ps1
+++ d/qiiwexc.ps1
@@ -1,4 +1,4 @@
-Set-Variable -Option Constant Version ([Version]'22.11.3')
+Set-Variable -Option Constant Version ([Version]'22.11.22')
 
 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-# Info #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
@@ -72,7 +72,7 @@ Set-Variable -Option Constant URL_WINDOWS_10 'https://w14.monkrus.ws/2022/11/win
 Set-Variable -Option Constant URL_WINDOWS_7  'https://w14.monkrus.ws/2022/02/windows-7-sp1-rus-eng-x86-x64-18in1.html'
 
 Set-Variable -Option Constant URL_RUFUS    'https://github.com/pbatard/rufus/releases/download/v3.20/rufus-3.20p.exe'
-Set-Variable -Option Constant URL_VENTOY   'https://github.com/ventoy/Ventoy/releases/download/v1.0.81/ventoy-1.0.81-windows.zip'
+Set-Variable -Option Constant URL_VENTOY   'https://github.com/ventoy/Ventoy/releases/download/v1.0.82/ventoy-1.0.82-windows.zip'
 Set-Variable -Option Constant URL_SDI      'https://sdi-tool.org/releases/SDI_R2201.zip'
 Set-Variable -Option Constant URL_VICTORIA 'https://hdd.by/Victoria/Victoria537.zip'
 
@@ -1408,7 +1408,6 @@ Function Start-DiskCleanup {
         "$env:ProgramFiles\VideoLAN\VLC\New_Skins.url"
         "$env:ProgramFiles\VideoLAN\VLC\NEWS.txt"
         "$env:ProgramFiles\VideoLAN\VLC\README.txt"
-        "$env:ProgramFiles\VideoLAN\VLC\THANKS.txt"
         "$env:ProgramFiles\VideoLAN\VLC\VideoLAN Website.url"
         "$env:ProgramFiles\WinRAR\Descript.ion"
         "$env:ProgramFiles\WinRAR\Order.htm"
--- src/0 Setup/02 URLs.ps1
+++ src/0 Setup/02 URLs.ps1
@@ -3,6 +3,6 @@ Set-Variable -Option Constant URL_WINDOWS_10 'https://w14.monkrus.ws/2022/11/win
 Set-Variable -Option Constant URL_WINDOWS_7  'https://w14.monkrus.ws/2022/02/windows-7-sp1-rus-eng-x86-x64-18in1.html'
 
 Set-Variable -Option Constant URL_RUFUS    'https://github.com/pbatard/rufus/releases/download/v3.20/rufus-3.20p.exe'
-Set-Variable -Option Constant URL_VENTOY   'https://github.com/ventoy/Ventoy/releases/download/v1.0.81/ventoy-1.0.81-windows.zip'
+Set-Variable -Option Constant URL_VENTOY   'https://github.com/ventoy/Ventoy/releases/download/v1.0.82/ventoy-1.0.82-windows.zip'
 Set-Variable -Option Constant URL_SDI      'https://sdi-tool.org/releases/SDI_R2201.zip'
 Set-Variable -Option Constant URL_VICTORIA 'https://hdd.by/Victoria/Victoria537.zip'
