db5075327efaa15bc0754275e0a2dd08066c8ea1
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2022-12-08 22:06:41 +0200

    Update Rufus 3.20 -> 3.21, update Ventoy 1.0.82 -> 1.0.84

--- src/0 Setup/02 URLs.ps1
+++ src/0 Setup/02 URLs.ps1
@@ -2,7 +2,7 @@ Set-Variable -Option Constant URL_WINDOWS_11 'https://w14.monkrus.ws/2022/10/win
 Set-Variable -Option Constant URL_WINDOWS_10 'https://w14.monkrus.ws/2022/11/windows-10-v22h2-rus-eng-x86-x64-32in1.html'
 Set-Variable -Option Constant URL_WINDOWS_7  'https://w14.monkrus.ws/2022/02/windows-7-sp1-rus-eng-x86-x64-18in1.html'
 
-Set-Variable -Option Constant URL_RUFUS    'https://github.com/pbatard/rufus/releases/download/v3.20/rufus-3.20p.exe'
-Set-Variable -Option Constant URL_VENTOY   'https://github.com/ventoy/Ventoy/releases/download/v1.0.82/ventoy-1.0.82-windows.zip'
+Set-Variable -Option Constant URL_RUFUS    'https://github.com/pbatard/rufus/releases/download/v3.21/rufus-3.21p.exe'
+Set-Variable -Option Constant URL_VENTOY   'https://github.com/ventoy/Ventoy/releases/download/v1.0.84/ventoy-1.0.84-windows.zip'
 Set-Variable -Option Constant URL_SDI      'https://sdi-tool.org/releases/SDI_R2201.zip'
 Set-Variable -Option Constant URL_VICTORIA 'https://hdd.by/Victoria/Victoria537.zip'
