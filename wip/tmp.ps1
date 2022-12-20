96fd1dc3694eae3279382be6de8b54c17ffbaab8
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2022-12-20 14:56:35 +0200

    Minor cleanup

--- wip/tmp.ps1
+++ wip/tmp.ps1
@@ -1,16 +1,5 @@
 .'Computer\HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies' 'Attachments' 'SaveZoneInformation' DWORD:1
 
-Function Start-DiskCheck {
-    Set-Variable -Option Constant Parameters ('`$(`$Item.Name + ":")' + $(if ($FullScan) { ' /B' } elseif ($OS_VERSION -gt 7) { ' /scan /perf' }))
-    Set-Variable -Option Constant Command "ForEach (`$Item in Get-PSDrive -PSProvider 'FileSystem') { Write-Host `"``n``nScanning drive `$(`$Item.Name + ':')``n``n`"; Start-Process 'chkdsk' '$Parameters'; Start-Sleep 5 }"
-
-    try { Start-Process 'PowerShell' "-Command `"(Get-Host).UI.RawUI.WindowTitle = 'Disk check running...'; $Command`"" -Verb RunAs }
-    catch [Exception] { Add-Log $ERR "Failed to check (C:) disk health: $($_.Exception.Message)"; Return }
-}
-
-# ForEach ($Item in Get-PSDrive -PSProvider 'FileSystem') { Write-Host "`n`nScanning drive $($Item.Name):`n`n"; Start-Process 'chkdsk' "$($Item.Name + ':') /scan /perf" -NoNewWindow -Wait }
-# $Parameters = '$($Item.Name + ":")' + $(if ($False) { ' /B' } elseif ($True) { ' /scan /perf' })
-
 OInstall
 /configure "d:\MyPath\Configuration.xml"    - Launch the program in hidden mode, and perform the installation.
 /proplus x64 en-us excludeExcel excludeOneNote /apps visio
