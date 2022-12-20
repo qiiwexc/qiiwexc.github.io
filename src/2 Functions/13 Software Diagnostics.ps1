e2e88bbd7681c588512534b20b355c0cddbbb8d3
qiiwexc <romans.ivanovs.x@gmail.com>
2022-12-20 14:56:35 +0200

    Minor cleanup

--- src/2 Functions/13 Software Diagnostics.ps1
+++ src/2 Functions/13 Software Diagnostics.ps1
@@ -1,3 +1,12 @@
+Function Start-DiskCleanup {
+    Add-Log $INF 'Starting disk cleanup utility...'
+
+    try { Start-Process -Verb RunAs 'cleanmgr' '/lowdisk' }
+    catch [Exception] { Add-Log $ERR "Failed to start disk cleanup utility: $($_.Exception.Message)"; Return }
+
+    Out-Success
+}
+
 Function Test-WindowsHealth {
     Add-Log $INF 'Starting Windows health check...'
 
