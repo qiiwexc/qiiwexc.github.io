7604250389f5f037de8a4e6a67439f737d275f04
qiiwexc <romans.ivanovs.x@gmail.com>
2023-02-18 20:43:03 +0200

    Minor refactoring

--- src/2 Functions/13 Software Diagnostics.ps1
+++ src/2 Functions/13 Software Diagnostics.ps1
@@ -10,10 +10,14 @@ Function Start-DiskCleanup {
 Function Test-WindowsHealth {
     Add-Log $INF 'Starting Windows health check...'
 
-    Set-Variable -Option Constant ScanHealth "Start-Process -NoNewWindow -Wait 'DISM' '/Online /Cleanup-Image /ScanHealth'"
-    Set-Variable -Option Constant RestoreHealth $(if ($OS_VERSION -gt 7) { "Start-Process -NoNewWindow -Wait 'DISM' '/Online /Cleanup-Image /RestoreHealth'" } else { '' })
+    Set-Variable -Option Constant Command $(if ($OS_VERSION -gt 7) {
+            "Start-Process -NoNewWindow -Wait 'DISM' '/Online /Cleanup-Image /RestoreHealth'"
+        }
+        else {
+            "Start-Process -NoNewWindow -Wait 'DISM' '/Online /Cleanup-Image /ScanHealth'"
+        })
 
-    try { Start-ExternalProcess -Elevated -Title:'Checking Windows health...' @($ScanHealth, $RestoreHealth) }
+    try { Start-ExternalProcess -Elevated -Title:'Checking Windows health...' $Command }
     catch [Exception] { Add-Log $ERR "Failed to check Windows health: $($_.Exception.Message)"; Return }
 
     Out-Success
