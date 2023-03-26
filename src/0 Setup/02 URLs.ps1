c88f00a4d3b15da4a234efe7ebd0971a301c0375
qiiwexc <romans.ivanovs.x@gmail.com>
2023-03-27 01:57:52 +0300

    Update Rufus to 3.22; Update Ventoy to 1.0.90; Update Windows 7 SP1

--- src/0 Setup/02 URLs.ps1
+++ src/0 Setup/02 URLs.ps1
@@ -1,8 +1,8 @@
 Set-Variable -Option Constant URL_WINDOWS_11 'https://w14.monkrus.ws/2022/10/windows-11-v22h2-rus-eng-28in1-hwid-act.html'
 Set-Variable -Option Constant URL_WINDOWS_10 'https://w14.monkrus.ws/2022/11/windows-10-v22h2-rus-eng-x86-x64-32in1.html'
-Set-Variable -Option Constant URL_WINDOWS_7  'https://w14.monkrus.ws/2022/02/windows-7-sp1-rus-eng-x86-x64-18in1.html'
+Set-Variable -Option Constant URL_WINDOWS_7  'https://w14.monkrus.ws/2023/03/windows-7-sp1-rus-eng-x86-x64-18in1.html'
 
-Set-Variable -Option Constant URL_RUFUS    'https://github.com/pbatard/rufus/releases/download/v3.21/rufus-3.21p.exe'
-Set-Variable -Option Constant URL_VENTOY   'https://github.com/ventoy/Ventoy/releases/download/v1.0.89/ventoy-1.0.89-windows.zip'
+Set-Variable -Option Constant URL_RUFUS    'https://github.com/pbatard/rufus/releases/download/v3.22/rufus-3.22p.exe'
+Set-Variable -Option Constant URL_VENTOY   'https://github.com/ventoy/Ventoy/releases/download/v1.0.90/ventoy-1.0.90-windows.zip'
 Set-Variable -Option Constant URL_SDIO     'https://www.glenn.delahoy.com/downloads/sdio/SDIO_1.12.9.749.zip'
 Set-Variable -Option Constant URL_VICTORIA 'https://hdd.by/Victoria/Victoria537.zip'
