d870eebea9be54392a09f86c587ecb68d4eb1938
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2023-08-16 15:22:29 +0300

    Update SDIO 1.12.14.755 -> 1.12.15.756

--- src/0 Setup/02 URLs.ps1
+++ src/0 Setup/02 URLs.ps1
@@ -4,5 +4,5 @@ Set-Variable -Option Constant URL_WINDOWS_7  'https://w14.monkrus.ws/2023/03/win
 
 Set-Variable -Option Constant URL_RUFUS    'https://github.com/pbatard/rufus/releases/download/v4.2/rufus-4.2p.exe'
 Set-Variable -Option Constant URL_VENTOY   'https://github.com/ventoy/Ventoy/releases/download/v1.0.94/ventoy-1.0.94-windows.zip'
-Set-Variable -Option Constant URL_SDIO     'https://www.glenn.delahoy.com/downloads/sdio/SDIO_1.12.14.755.zip'
+Set-Variable -Option Constant URL_SDIO     'https://www.glenn.delahoy.com/downloads/sdio/SDIO_1.12.15.756.zip'
 Set-Variable -Option Constant URL_VICTORIA 'https://hdd.by/Victoria/Victoria537.zip'
