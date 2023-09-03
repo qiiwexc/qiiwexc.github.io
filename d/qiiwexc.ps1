c929160d5ab1465ea0840263a49960938e4357e2
qiiwexc <romans.ivanovs.x@gmail.com>
2023-09-03 12:23:33 +0300

    Update KMSAuto Lite 1.7.8 -> 1.8.0, Update AAct 4.3.0 -> 4.3.1

--- d/qiiwexc.ps1
+++ d/qiiwexc.ps1
@@ -1,4 +1,4 @@
-Set-Variable -Option Constant Version ([Version]'23.8.26')
+Set-Variable -Option Constant Version ([Version]'23.9.1')
 
 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-# Info #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
