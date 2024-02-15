539770e8ed1b9a82960729fdf5c83f9936ba40e2
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2024-02-15 20:46:21 +0200

    Update Windows 7 SP1 UnsupportEd v2 -> v3

--- d/qiiwexc.ps1
+++ d/qiiwexc.ps1
@@ -1,4 +1,4 @@
-Set-Variable -Option Constant Version ([Version]'24.2.6')
+Set-Variable -Option Constant Version ([Version]'24.2.15')
 
 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-# Info #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
@@ -66,7 +66,7 @@ Set-Variable -Option Constant REQUIRES_ELEVATION $(if (!$IS_ELEVATED) { ' *' } e
 
 Set-Variable -Option Constant URL_WINDOWS_11 'https://w14.monkrus.ws/2024/01/windows-11-v23h2-rus-eng-28in1-hwid-act.html'
 Set-Variable -Option Constant URL_WINDOWS_10 'https://w14.monkrus.ws/2022/11/windows-10-v22h2-rus-eng-x86-x64-32in1.html'
-Set-Variable -Option Constant URL_WINDOWS_7  'https://w14.monkrus.ws/2023/03/windows-7-sp1-rus-eng-x86-x64-18in1.html'
+Set-Variable -Option Constant URL_WINDOWS_7  'https://w14.monkrus.ws/2024/02/windows-7-sp1-rus-eng-x86-x64-18in1.html'
 
 Set-Variable -Option Constant URL_RUFUS    'https://github.com/pbatard/rufus/releases/download/v4.4/rufus-4.4p.exe'
 Set-Variable -Option Constant URL_VENTOY   'https://github.com/ventoy/Ventoy/releases/download/v1.0.97/ventoy-1.0.97-windows.zip'
