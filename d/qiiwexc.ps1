5ab68b186a32d9c3151413a61a87f03df94fd2d3
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2024-05-03 08:44:35 +0300

    Update KMSAuto Lite 1.8.6 -> 1.8.8; Office C2R 7.7.7.6 -> 7.7.7.7 r6

--- d/qiiwexc.ps1
+++ d/qiiwexc.ps1
@@ -1,4 +1,4 @@
-Set-Variable -Option Constant Version ([Version]'24.3.19')
+Set-Variable -Option Constant Version ([Version]'24.5.2')
 
 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-# Info #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
