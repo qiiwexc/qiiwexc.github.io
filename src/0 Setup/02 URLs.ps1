b03d235daab81ce5af7df511ab4690bcd4ce290d
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2024-12-30 19:11:38 +0200

    Fix Windows download URLs

--- src/0 Setup/02 URLs.ps1
+++ src/0 Setup/02 URLs.ps1
@@ -1,6 +1,6 @@
-Set-Variable -Option Constant URL_WINDOWS_11 'https://w14.monkrus.ws/2024/01/windows-11-v23h2-rus-eng-28in1-hwid-act.html'
-Set-Variable -Option Constant URL_WINDOWS_10 'https://w14.monkrus.ws/2022/11/windows-10-v22h2-rus-eng-x86-x64-32in1.html'
-Set-Variable -Option Constant URL_WINDOWS_7  'https://w14.monkrus.ws/2024/02/windows-7-sp1-rus-eng-x86-x64-18in1.html'
+Set-Variable -Option Constant URL_WINDOWS_11 'https://w16.monkrus.ws/2024/01/windows-11-v23h2-rus-eng-28in1-hwid-act.html'
+Set-Variable -Option Constant URL_WINDOWS_10 'https://w16.monkrus.ws/2022/11/windows-10-v22h2-rus-eng-x86-x64-32in1.html'
+Set-Variable -Option Constant URL_WINDOWS_7  'https://w16.monkrus.ws/2024/02/windows-7-sp1-rus-eng-x86-x64-18in1.html'
 
 Set-Variable -Option Constant URL_RUFUS    'https://github.com/pbatard/rufus/releases/download/v4.6/rufus-4.6p.exe'
 Set-Variable -Option Constant URL_VENTOY   'https://github.com/ventoy/Ventoy/releases/download/v1.0.99/ventoy-1.0.99-windows.zip'
