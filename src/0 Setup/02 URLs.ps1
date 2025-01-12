e311f0bf0f075042362c5ef2f839e4c35942e114
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2025-01-12 20:48:19 +0200

    Upgrade Windows 11 v23H2 -> v24H2

--- src/0 Setup/02 URLs.ps1
+++ src/0 Setup/02 URLs.ps1
@@ -1,4 +1,4 @@
-Set-Variable -Option Constant URL_WINDOWS_11 'https://w16.monkrus.ws/2024/01/windows-11-v23h2-rus-eng-28in1-hwid-act.html'
+Set-Variable -Option Constant URL_WINDOWS_11 'https://w16.monkrus.ws/2025/01/windows-11-v24h2-rus-eng-20in1-hwid-act.html'
 Set-Variable -Option Constant URL_WINDOWS_10 'https://w16.monkrus.ws/2022/11/windows-10-v22h2-rus-eng-x86-x64-32in1.html'
 Set-Variable -Option Constant URL_WINDOWS_7  'https://w16.monkrus.ws/2024/02/windows-7-sp1-rus-eng-x86-x64-18in1.html'
 
