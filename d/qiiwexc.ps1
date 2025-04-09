4e5de13a8a37eaf9bca73cfdd5be1e1a39b35b91
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2025-04-10 02:22:56 +0300

    Update Rufus v4.6 -> v4.7

--- d/qiiwexc.ps1
+++ d/qiiwexc.ps1
@@ -1,4 +1,4 @@
-Set-Variable -Option Constant Version ([Version]'25.2.28')
+Set-Variable -Option Constant Version ([Version]'25.4.10')
 
 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-# Info #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
@@ -68,7 +68,7 @@ Set-Variable -Option Constant URL_WINDOWS_11 'https://w16.monkrus.ws/2025/01/win
 Set-Variable -Option Constant URL_WINDOWS_10 'https://w16.monkrus.ws/2022/11/windows-10-v22h2-rus-eng-x86-x64-32in1.html'
 Set-Variable -Option Constant URL_WINDOWS_7  'https://w16.monkrus.ws/2024/02/windows-7-sp1-rus-eng-x86-x64-18in1.html'
 
-Set-Variable -Option Constant URL_RUFUS    'https://github.com/pbatard/rufus/releases/download/v4.6/rufus-4.6p.exe'
+Set-Variable -Option Constant URL_RUFUS    'https://github.com/pbatard/rufus/releases/download/v4.7/rufus-4.7p.exe'
 Set-Variable -Option Constant URL_VENTOY   'https://github.com/ventoy/Ventoy/releases/download/v1.1.05/ventoy-1.1.05-windows.zip'
 Set-Variable -Option Constant URL_SDIO     'https://www.glenn.delahoy.com/downloads/sdio/SDIO_1.15.1.813.zip'
 Set-Variable -Option Constant URL_VICTORIA 'https://hdd.by/Victoria/Victoria537.zip'
