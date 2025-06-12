f215a394a87a2749c2ea910739db16ef4d816075
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2025-06-12 09:33:41 +0300

    Update Rufus v4.7 -> v4.8

--- src/0 Setup/02 URLs.ps1
+++ src/0 Setup/02 URLs.ps1
@@ -2,7 +2,7 @@ Set-Variable -Option Constant URL_WINDOWS_11 'https://w16.monkrus.ws/2025/01/win
 Set-Variable -Option Constant URL_WINDOWS_10 'https://w16.monkrus.ws/2022/11/windows-10-v22h2-rus-eng-x86-x64-32in1.html'
 Set-Variable -Option Constant URL_WINDOWS_7  'https://w16.monkrus.ws/2024/02/windows-7-sp1-rus-eng-x86-x64-18in1.html'
 
-Set-Variable -Option Constant URL_RUFUS    'https://github.com/pbatard/rufus/releases/download/v4.7/rufus-4.7p.exe'
+Set-Variable -Option Constant URL_RUFUS    'https://github.com/pbatard/rufus/releases/download/v4.8/rufus-4.8p.exe'
 Set-Variable -Option Constant URL_VENTOY   'https://github.com/ventoy/Ventoy/releases/download/v1.1.05/ventoy-1.1.05-windows.zip'
 Set-Variable -Option Constant URL_SDIO     'https://www.glenn.delahoy.com/downloads/sdio/SDIO_1.15.4.815.zip'
 Set-Variable -Option Constant URL_VICTORIA 'https://hdd.by/Victoria/Victoria537.zip'
