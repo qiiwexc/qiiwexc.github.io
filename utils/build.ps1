c2a3cfd0c8f705eb6367cb4aee0805b57252e2d9
qiiwexc <romans.ivanovs.x@gmail.com>
2025-08-17 17:48:48 +0300

    Add ps1 script signature to the batch script

--- utils/build.ps1
+++ utils/build.ps1
@@ -1,22 +1,43 @@
-Set-Variable -Option Constant Version     (Get-Date -Format 'y.M.d')
-Set-Variable -Option Constant ProjectName 'qiiwexc'
-
-Set-Variable -Option Constant SourcePath '.\src'
-Set-Variable -Option Constant DistPath   '.\d'
-
-Set-Variable -Option Constant VersionFile     "$DistPath\version"
-Set-Variable -Option Constant OutputFile      "$DistPath\$ProjectName.ps1"
-Set-Variable -Option Constant BatchFile       "$DistPath\$ProjectName.bat"
-Set-Variable -Option Constant ThumbprintPath  ".\certificate\$ProjectName.txt"
-Set-Variable -Option Constant TimestampServer 'http://timestamp.digicert.com'
-
-Set-Variable -Option Constant WebPageFile ".\index.html"
-Set-Variable -Option Constant HtmlTitle   "<title>$ProjectName $Version</title>"
-Set-Variable -Option Constant HtmlHeader  "<h2><a href=`"d/$ProjectName.bat`">$ProjectName $Version</a></h2>"
-
 Set-Variable -Option Constant INF 'INF'
 Set-Variable -Option Constant WRN 'WRN'
 Set-Variable -Option Constant ERR 'ERR'
+
+
+Function Start-Build {
+    Param([Switch]$AndRun)
+
+    Set-Variable -Option Constant Version     (Get-Date -Format 'y.M.d')
+    Set-Variable -Option Constant ProjectName 'qiiwexc'
+
+    Set-Variable -Option Constant SourcePath '.\src'
+    Set-Variable -Option Constant DistPath   '.\d'
+
+    Set-Variable -Option Constant VersionFile "$DistPath\version"
+    Set-Variable -Option Constant Ps1File     "$DistPath\$ProjectName.ps1"
+    Set-Variable -Option Constant BatchFile   "$DistPath\$ProjectName.bat"
+
+    Add-Log $INF 'Build task started'
+    Add-Log $INF "Version     = $Version"
+    Add-Log $INF "Source path = $SourcePath"
+    Add-Log $INF "Output file = $Ps1File"
+
+    Write-VersionFile $Version $VersionFile
+
+    Update-Html $ProjectName $Version
+
+    New-PowerShell $Version $SourcePath $Ps1File
+
+    Set-Signature $Ps1File $ProjectName
+
+    New-Batch $Ps1File $BatchFile
+
+    Add-Log $INF 'Build finished'
+
+    if ($AndRun) {
+        Add-Log $INF "Running $Ps1File"
+        Start-Process 'PowerShell' ".\$Ps1File"
+    }
+}
 
 
 Function Add-Log {
@@ -41,40 +62,37 @@ Function Add-Log {
 }
 
 
-Function Start-Build {
-    Param([Switch]$AndRun)
+Function Write-VersionFile {
+    Param(
+        [String][Parameter(Position = 0, Mandatory = $True)]$Version,
+        [String][Parameter(Position = 1, Mandatory = $True)]$VersionFile
+    )
 
-    Add-Log $INF 'Build task started'
-    Add-Log $INF "Version     = $Version"
-    Add-Log $INF "Source path = $SourcePath"
-    Add-Log $INF "Output file = $OutputFile"
+    Add-Log $INF 'Writing version file...'
 
     Remove-Item -Force -ErrorAction SilentlyContinue $VersionFile
-    Remove-Item -Force -ErrorAction SilentlyContinue $OutputFile
-    Remove-Item -Force -ErrorAction SilentlyContinue $BatchFile
 
-    New-Item -Force -ItemType Directory $DistPath | Out-Null
-
-    Add-Log $INF 'Writing version file'
     Set-Content $VersionFile "$Version`n" -NoNewline
 
-    Add-Log $INF 'Updating version on the web page'
+    Add-Log $INF 'Done'
+}
+
+
+Function Update-Html {
+    Param(
+        [String][Parameter(Position = 0, Mandatory = $True)]$ProjectName,
+        [String][Parameter(Position = 1, Mandatory = $True)]$Version
+    )
+
+    Add-Log $INF 'Updating version on the web page...'
+
+    Set-Variable -Option Constant WebPageFile ".\index.html"
+    Set-Variable -Option Constant HtmlTitle   "<title>$ProjectName $Version</title>"
+    Set-Variable -Option Constant HtmlHeader  "<h2><a href=`"d/$ProjectName.bat`">$ProjectName $Version</a></h2>"
+
     (Get-Content $WebPageFile) | ForEach-Object { $_ -Replace "<title>.+", $HtmlTitle -Replace "<h2>.+", $HtmlHeader } | Set-Content $WebPageFile
 
-    Add-Log $INF 'Building...'
-
-    [String[]]$PowerShellStrings = New-PowerShell $Version $SourcePath $OutputFile
-
-    New-Batch $PowerShellStrings
-
-    Set-Signature $OutputFile $ThumbprintPath
-
-    Add-Log $INF 'Finished'
-
-    if ($AndRun) {
-        Add-Log $INF "Running $OutputFile"
-        Start-Process 'PowerShell' ".\$OutputFile"
-    }
+    Add-Log $INF 'Done'
 }
 
 
@@ -82,8 +100,14 @@ Function New-PowerShell {
     Param(
         [String][Parameter(Position = 0, Mandatory = $True)]$Version,
         [String][Parameter(Position = 1, Mandatory = $True)]$SourcePath,
-        [String][Parameter(Position = 2, Mandatory = $True)]$OutputFile
+        [String][Parameter(Position = 2, Mandatory = $True)]$Ps1File
     )
+
+    Add-Log $INF 'Building PowerShell script...'
+
+    Remove-Item -Force -ErrorAction SilentlyContinue $Ps1File
+
+    New-Item -Force -ItemType Directory $DistPath | Out-Null
 
     [String[]]$OutputStrings = "Set-Variable -Option Constant Version ([Version]'$Version')"
 
@@ -98,17 +122,24 @@ Function New-PowerShell {
         }
     }
 
-    Add-Log $INF "Writing output file $OutputFile"
-    $OutputStrings | Out-File $OutputFile -Encoding ASCII
+    Add-Log $INF "Writing output file $Ps1File"
+    $OutputStrings | Out-File $Ps1File -Encoding ASCII
 
-    Return $OutputStrings
+    Add-Log $INF 'Done'
 }
 
 
 Function New-Batch {
     Param(
-        [String[]][Parameter(Position = 0)]$PowerShellStrings
+        [String][Parameter(Position = 0, Mandatory = $True)]$Ps1File,
+        [String][Parameter(Position = 1, Mandatory = $True)]$BatchFile
     )
+
+    Add-Log $INF 'Building batch script...'
+
+    Remove-Item -Force -ErrorAction SilentlyContinue $BatchFile
+
+    [String[]]$PowerShellStrings = Get-Content $Ps1File
 
     [String[]]$BatchStrings = "@echo off`n"
     $BatchStrings += "set `"psfile=%temp%\$ProjectName.ps1`"`n"
@@ -129,23 +160,28 @@ Function New-Batch {
 
     Add-Log $INF "Writing batch file $BatchFile"
     $BatchStrings | Out-File $BatchFile -Encoding ASCII
+
+    Add-Log $INF 'Done'
 }
 
 Function Set-Signature {
     Param(
-        [String][Parameter(Position = 0, Mandatory = $True)]$OutputFile,
-        [String][Parameter(Position = 1, Mandatory = $True)]$ThumbprintPath
+        [String][Parameter(Position = 0, Mandatory = $True)]$Ps1File,
+        [String][Parameter(Position = 1, Mandatory = $True)]$ProjectName
     )
 
     Add-Log $INF 'Signing...'
+
+    Set-Variable -Option Constant ThumbprintPath ".\certificate\$ProjectName.txt"
+    Set-Variable -Option Constant TimestampServer 'http://timestamp.digicert.com'
 
     [String]$Thumbprint = Get-Content $ThumbprintPath
 
     $CodeSignCert = Get-ChildItem -Path Cert:\CurrentUser\My | Where-Object {$_.Thumbprint -eq $Thumbprint}
 
-    Set-AuthenticodeSignature -FilePath $OutputFile -Certificate $CodeSignCert -TimestampServer $TimestampServer
+    Set-AuthenticodeSignature -FilePath $Ps1File -Certificate $CodeSignCert -TimestampServer $TimestampServer | Out-Null
 
-    Add-Log $INF 'Signed'
+    Add-Log $INF 'Done'
 }
 
 
