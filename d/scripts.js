13424f0f4719b79f620b2a5c85a0641dda18bf9a
qiiwexc <romans.ivanovs.x@gmail.com>
2025-08-17 18:01:43 +0300

    Minor webpage refactoring

--- d/scripts.js
+++ d/scripts.js
@@ -1,30 +1,42 @@
-if (navigator.appVersion.indexOf('Windows NT 10.0.2') !== -1) {
-    var systemVersion = 'Windows 11';
-} else if (navigator.appVersion.indexOf('Windows NT 10.0') !== -1) {
-    var systemVersion = 'Windows 10';
-} else if (navigator.appVersion.indexOf('Windows NT 6.4') !== -1) {
-    var systemVersion = 'Windows 10';
-} else if (navigator.appVersion.indexOf('Windows NT 6.3') !== -1) {
-    var systemVersion = 'Windows 8.1';
-} else if (navigator.appVersion.indexOf('Windows NT 6.2') !== -1) {
-    var systemVersion = 'Windows 8';
-} else if (navigator.appVersion.indexOf('Windows NT 6.1') !== -1) {
-    var systemVersion = 'Windows 7';
-} else if (navigator.appVersion.indexOf('Windows NT 6.0') !== -1) {
-    var systemVersion = 'Windows Vista';
-} else if (navigator.appVersion.indexOf('Windows NT 5.2') !== -1) {
-    var systemVersion = 'Windows XP';
-} else if (navigator.appVersion.indexOf('Windows NT 5.1') !== -1) {
-    var systemVersion = 'Windows XP';
-} else if (navigator.appVersion.indexOf('Windows NT 5.0') !== -1) {
-    var systemVersion = 'Windows 2000';
-} else if (navigator.appVersion.indexOf('Windows NT 4.0') !== -1) {
-    var systemVersion = 'Windows NT';
-} else {
-    var systemVersion = 'Unknown Operating System';
+let systemVersion = "Unknown Operating System";
+
+if (navigator.userAgent.includes("Windows NT 10.0")) {
+  systemVersion = "Windows 10/11";
+} else if (navigator.userAgent.includes("Windows NT 6.4")) {
+  systemVersion = "Windows 10";
+} else if (navigator.userAgent.includes("Windows NT 6.3")) {
+  systemVersion = "Windows 8.1";
+} else if (navigator.userAgent.includes("Windows NT 6.2")) {
+  systemVersion = "Windows 8";
+} else if (navigator.userAgent.includes("Windows NT 6.1")) {
+  systemVersion = "Windows 7";
+} else if (navigator.userAgent.includes("Windows NT 6.0")) {
+  systemVersion = "Windows Vista";
+} else if (navigator.userAgent.includes("Windows NT 5.2")) {
+  systemVersion = "Windows XP";
+} else if (navigator.userAgent.includes("Windows NT 5.1")) {
+  systemVersion = "Windows XP";
+} else if (navigator.userAgent.includes("Windows NT 5.0")) {
+  systemVersion = "Windows 2000";
+} else if (navigator.userAgent.includes("Windows NT 4.0")) {
+  systemVersion = "Windows NT";
 }
 
-var systemArchitecture = navigator.userAgent.indexOf('WOW64') !== -1 || navigator.userAgent.indexOf('Win64') !== -1 ? ' x64 ' : ' x86 ';
-var systemLanguage = (navigator.browserLanguage ? navigator.browserLanguage : navigator.language).indexOf('ru') !== -1 ? 'RU' : 'EN';
+let systemArchitecture = ["Win64", "x64", "WOW64"].some((str) =>
+  navigator.userAgent.includes(str)
+)
+  ? "64-bit"
+  : "32-bit";
 
-document.getElementsByTagName('p')[0].innerHTML = (systemVersion + systemArchitecture + systemLanguage);
+let systemLanguage = navigator.language.split("-").at(0) ?? "en";
+
+let versionElement = document
+  .getElementsByTagName("header")
+  .item(0)
+  ?.getElementsByTagName("p")
+  .item(0);
+
+if (versionElement) {
+  versionElement.innerHTML =
+    systemVersion + " " + systemArchitecture + " " + systemLanguage;
+}
