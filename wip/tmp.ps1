d6e8be075181daa627f7e4dce42dc6b90bd82215
qiiwexc <romans.ivanovs.x@gmail.com>
2025-09-06 23:20:42 +0300

    Remove some wip files

--- wip/tmp.ps1
+++ wip/tmp.ps1
@@ -1,9 +1,3 @@
 .'Computer\HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies' 'Attachments' 'SaveZoneInformation' DWORD:1
 
-OInstall
-/configure "d:\MyPath\Configuration.xml"    - Launch the program in hidden mode, and perform the installation.
-/proplus x64 en-us excludeExcel excludeOneNote /apps visio
-/apps x64 ru-ru en-us word excel visio
-/proplus x86 en-us excludeOneNote /apps project /convert /activate
-
 New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main' -Name 'DisableFirstRunCustomize' -Value 2 -PropertyType DWORD -Force | Out-Null
