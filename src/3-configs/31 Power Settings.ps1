76a2d80deb1935bb7703a80a89c8da9c90fd7bc3
qiiwexc <romans.ivanovs.x@gmail.com>
2025-09-23 15:10:37 +0300

    Refactor Windows power configuration settings

---
+++ src/3-configs/31 Power Settings.ps1
@@ -0,0 +1,12 @@
+Set-Variable -Option Constant CONFIG_POWER_SETTINGS @(
+    @{SubGroup='0d7dbae2-4294-402a-ba8e-26777e8488cd'; Setting='309dce9b-bef4-4119-9921-a851fb12f0f4'; Value=0},
+    @{SubGroup='02f815b5-a5cf-4c84-bf20-649d1f75d3d8'; Setting='4c793e7d-a264-42e1-87d3-7a0d2f523ccd'; Value=1},
+    @{SubGroup='19cbb8fa-5279-450e-9fac-8a3d5fedd0c1'; Setting='12bbebe6-58d6-4636-95bb-3217ef867c1a'; Value=0},
+    @{SubGroup='9596fb26-9850-41fd-ac3e-f7c3c00afd4b'; Setting='34c7b99f-9a6d-4b3c-8dc7-b6693b78cef4'; Value=0},
+    @{SubGroup='9596fb26-9850-41fd-ac3e-f7c3c00afd4b'; Setting='03680956-93bc-4294-bba6-4e0f09bb717f'; Value=1},
+    @{SubGroup='9596fb26-9850-41fd-ac3e-f7c3c00afd4b'; Setting='10778347-1370-4ee0-8bbd-33bdacaade49'; Value=1},
+    @{SubGroup='de830923-a562-41af-a086-e3a2c6bad2da'; Setting='e69653ca-cf7f-4f05-aa73-cb833fa90ad4'; Value=0},
+    @{SubGroup='SUB_PCIEXPRESS'; Setting='ASPM'; Value=0},
+    @{SubGroup='SUB_SLEEP'; Setting='HYBRIDSLEEP'; Value=1},
+    @{SubGroup='SUB_SLEEP'; Setting='RTCWAKE'; Value=1}
+)
