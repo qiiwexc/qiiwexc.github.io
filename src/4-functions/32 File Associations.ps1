864cf40f840d1dd96d98baba4ea13ea9c9818b11
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2025-09-27 00:00:57 +0300

    Add the logic for setting file associations

---
+++ src/4-functions/32 File Associations.ps1
@@ -0,0 +1,24 @@
+function Set-FileAssociations {
+    Write-Log $INF 'Setting file associations...'
+
+    Set-Variable -Option Constant ScriptPath "$PATH_TEMP_DIR\Sophia.ps1"
+
+    $Parameters = @{
+        Uri             = "https://raw.githubusercontent.com/farag2/Sophia-Script-for-Windows/master/src/Sophia_Script_for_Windows_$OS_VERSION/Module/Sophia.psm1"
+        Outfile         = $ScriptPath
+        UseBasicParsing = $true
+    }
+    Invoke-WebRequest @Parameters
+
+    (Get-Content -Path $ScriptPath -Force) | Set-Content -Path $ScriptPath -Encoding UTF8 -Force
+
+    . $ScriptPath
+
+    foreach ($FileAssociation in $CONFIG_FILE_ASSOCIATIONS) {
+        Set-Association -ProgramPath $FileAssociation.Application -Extension $FileAssociation.Extension
+    }
+
+    Remove-Item -Path $ScriptPath -Force
+
+    Out-Success
+}
