8af2422cbce47975076cc03659eca92700d1b885
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2025-09-26 23:38:02 +0300

    Refactor functions

---
+++ src/4-functions/10 Exit.ps1
@@ -0,0 +1,11 @@
+function Reset-State {
+    Write-Log $INF "Cleaning up '$PATH_APP_DIR'"
+    Remove-Item -Force -ErrorAction SilentlyContinue -Recurse $PATH_APP_DIR
+    $HOST.UI.RawUI.WindowTitle = $OLD_WINDOW_TITLE
+    Write-Host ''
+}
+
+function Exit-Script {
+    Reset-State
+    $FORM.Close()
+}
