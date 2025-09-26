2f9578920e46c63236ebc78b838e7e5a19ccc58f
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2025-09-26 23:08:21 +0300

    Refactor system information fetching

--- src/0-init/04 Initialization.ps1
+++ src/0-init/04 Initialization.ps1
@@ -29,11 +29,13 @@ Set-Variable -Option Constant PATH_APP_DIR "$PATH_TEMP_DIR\qiiwexc"
 Set-Variable -Option Constant PATH_OFFICE_C2R_CLIENT_EXE "$env:CommonProgramFiles\Microsoft Shared\ClickToRun\OfficeC2RClient.exe"
 
 
+Set-Variable -Option Constant IS_LAPTOP ((Get-CimInstance -Class Win32_ComputerSystem -Property PCSystemType).PCSystemType -eq 2)
+
 Set-Variable -Option Constant SYSTEM_LANGUAGE (Get-SystemLanguage)
 
 Set-Variable -Option Constant PS_VERSION $PSVersionTable.PSVersion.Major
 
-Set-Variable -Option Constant OPERATING_SYSTEM (Get-WmiObject Win32_OperatingSystem | Select-Object Caption, Version)
+Set-Variable -Option Constant OPERATING_SYSTEM (Get-CimInstance Win32_OperatingSystem | Select-Object Caption, Version, OSArchitecture)
 Set-Variable -Option Constant IsWindows11 ($OPERATING_SYSTEM.Caption -match 'Windows 11')
 Set-Variable -Option Constant OS_VERSION $(if ($IsWindows11) { 11 } else { switch -Wildcard ($OS_BUILD) { '10.0.*' { 10 } '6.3.*' { 8.1 } '6.2.*' { 8 } '6.1.*' { 7 } Default { 'Vista or less / Unknown' } } })
 
--- src/2-ui/43 Battery Report.ps1
+++ src/2-ui/43 Battery Report.ps1
@@ -2,4 +2,4 @@ New-GroupBox 'Battery report'
 
 
 [ScriptBlock]$BUTTON_FUNCTION = { Get-BatteryReport }
-New-Button 'Get battery report' $BUTTON_FUNCTION
+New-Button 'Get battery report' $BUTTON_FUNCTION -Disabled:(!$IS_LAPTOP)
