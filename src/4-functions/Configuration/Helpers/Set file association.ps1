7a353a49e19ac8ff67a653bdf29e0dcc3dd5d779
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2025-09-30 02:08:28 +0300

    Update the logic for setting file associations

---
+++ src/4-functions/Configuration/Helpers/Set file association.ps1
@@ -0,0 +1,41 @@
+function Set-FileAssociation {
+    param(
+        [String][Parameter(Position = 0, Mandatory = $True)]$Application,
+        [String][Parameter(Position = 1, Mandatory = $True)]$RegistryPath,
+        [Switch][Parameter(Position = 2)]$SetDefault
+    )
+
+    if (-not (Test-Path $RegistryPath)) {
+        New-Item $RegistryPath
+    }
+
+    if ($SetDefault) {
+        Set-Variable -Option Constant DefaultAssociation (Get-ItemProperty -Path $RegistryPath).'(Default)'
+        if ($DefaultAssociation -ne $Application) {
+            Set-ItemProperty -Path $RegistryPath -Name '(Default)' -Value $Application
+        }
+    }
+
+    Set-Variable -Option Constant OpenWithProgidsPath "$RegistryPath\OpenWithProgids"
+    if (-not (Test-Path $OpenWithProgidsPath)) {
+        New-Item $OpenWithProgidsPath
+    }
+
+    Set-Variable -Option Constant OpenWithProgids (Get-ItemProperty -Path $OpenWithProgidsPath)
+    if ($OpenWithProgids) {
+        Set-Variable -Option Constant OpenWithProgidsNames ($OpenWithProgids | Get-Member -MemberType NoteProperty).Name
+        Set-Variable -Option Constant Progids ($OpenWithProgidsNames | Where-Object { $_ -ne 'PSDrive' -and $_ -ne 'PSProvider' -and $_ -ne 'PSPath' -and $_ -ne 'PSParentPath' -and $_ -ne 'PSChildName' })
+
+        foreach ($Progid in $Progids) {
+            if ($Progid -ne $Application) {
+                Remove-ItemProperty -Path $OpenWithProgidsPath -Name $Progid
+            }
+        }
+
+        if (-not ($Progids -contains $Application)) {
+            New-ItemProperty -Path $OpenWithProgidsPath -Name $Application -Value ''
+        }
+    } else {
+        New-ItemProperty -Path $OpenWithProgidsPath -Name $Application -Value ''
+    }
+}
