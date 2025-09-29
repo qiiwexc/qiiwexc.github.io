dff7e63ba8a4d3f3edd51bc027919111fcb1b926
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2025-09-30 02:08:28 +0300

    Update the logic for setting file associations

--- src/4-functions/Configuration/Windows/File associations.ps1
+++ src/4-functions/Configuration/Windows/File associations.ps1
@@ -1,24 +1,34 @@
 function Set-FileAssociations {
     Write-LogInfo 'Setting file associations...'
 
-    Set-Variable -Option Constant ScriptPath "$PATH_TEMP_DIR\Sophia.ps1"
+    Set-Variable -Option Constant SophiaScriptUrl "https://raw.githubusercontent.com/farag2/Sophia-Script-for-Windows/master/src/Sophia_Script_for_Windows_$OS_VERSION/Module/Sophia.psm1"
+    Set-Variable -Option Constant SophiaScriptPath "$PATH_TEMP_DIR\Sophia.ps1"
 
-    $Parameters = @{
-        Uri             = "https://raw.githubusercontent.com/farag2/Sophia-Script-for-Windows/master/src/Sophia_Script_for_Windows_$OS_VERSION/Module/Sophia.psm1"
-        Outfile         = $ScriptPath
-        UseBasicParsing = $True
-    }
-    Invoke-WebRequest @Parameters
+    Start-BitsTransfer -Source $SophiaScriptUrl -Destination $SophiaScriptPath -Dynamic
 
-    (Get-Content -Path $ScriptPath -Force) | Set-Content -Path $ScriptPath -Encoding UTF8 -Force
+    (Get-Content -Path $SophiaScriptPath -Force) | Set-Content -Path $SophiaScriptPath -Encoding UTF8 -Force
 
-    . $ScriptPath
+    . $SophiaScriptPath
 
     foreach ($FileAssociation in $CONFIG_FILE_ASSOCIATIONS) {
-        Set-Association -ProgramPath $FileAssociation.Application -Extension $FileAssociation.Extension
+        [String]$Extension = $FileAssociation.Extension
+        [String]$Application = $FileAssociation.Application
+
+        Set-FileAssociation $Application "Registry::HKEY_CLASSES_ROOT\$Extension"
+        Set-FileAssociation $Application "HKCU:\Software\Classes\$Extension" -SetDefault
+        Set-FileAssociation $Application "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\$Extension"
+
+        [String]$OriginalAssociation = $(& cmd.exe /c assoc $Extension 2`>`&1).Replace("$Extension=", '')
+        if ($OriginalAssociation -ne $Application) {
+            & cmd.exe /c assoc $Extension=$Application
+        }
+
+        if ($FileAssociation.Method -eq 'Sophia') {
+            Set-Association -ProgramPath $Application -Extension $Extension
+        }
     }
 
-    Remove-Item -Path $ScriptPath -Force
+    Remove-Item -Path $SophiaScriptPath -Force
 
     Out-Success
 }
