04b63ee3de0534cbe7e0d46fb247cbab8ada4981
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2025-09-30 14:45:25 +0300

    Check for internet connection before attempting to start external scripts

--- src/4-functions/Home/Activator.ps1
+++ src/4-functions/Home/Activator.ps1
@@ -1,6 +1,12 @@
 function Start-Activator {
     Write-LogInfo 'Starting MAS activator...'
 
+    Set-Variable -Option Constant NoConnection (Test-NetworkConnection)
+    if ($NoConnection) {
+        Write-LogError "Failed to start: $NoConnection"
+        return
+    }
+
     if ($OS_VERSION -eq 7) {
         Invoke-CustomCommand -HideWindow "iex ((New-Object Net.WebClient).DownloadString('https://get.activated.win'))"
     } else {
