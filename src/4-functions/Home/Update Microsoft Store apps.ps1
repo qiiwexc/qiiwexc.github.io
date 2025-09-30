bb6900e236c3741403d70bf757d7dfbf82cb2e4d
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2025-09-30 19:58:28 +0300

    Better error handling for Microsoft Store updates

--- src/4-functions/Home/Update Microsoft Store apps.ps1
+++ src/4-functions/Home/Update Microsoft Store apps.ps1
@@ -2,7 +2,11 @@ function Update-MicrosoftStoreApps {
     Write-LogInfo 'Starting Microsoft Store apps update...'
 
     try {
-        Invoke-CustomCommand -Elevated -HideWindow "Get-CimInstance MDM_EnterpriseModernAppManagement_AppManagement01 -Namespace 'root\cimv2\mdm\dmmap' | Invoke-CimMethod -MethodName 'UpdateScanMethod'"
+        $Status = (Get-CimInstance MDM_EnterpriseModernAppManagement_AppManagement01 -Namespace 'root\cimv2\mdm\dmmap' | Invoke-CimMethod -MethodName 'UpdateScanMethod').ReturnStatus
+
+        if ($Status -ne 0) {
+            Write-LogError 'Failed to update Microsoft Store apps'
+        }
     } catch [Exception] {
         Write-ExceptionLog $_ 'Failed to update Microsoft Store apps'
         return
