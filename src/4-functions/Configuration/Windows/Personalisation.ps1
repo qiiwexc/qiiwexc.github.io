4ea4fadc2f2e770754ae5296e9e39d03f9be2f42
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2025-10-02 00:30:52 +0300

    Update some configs

--- src/4-functions/Configuration/Windows/Personalisation.ps1
+++ src/4-functions/Configuration/Windows/Personalisation.ps1
@@ -5,6 +5,8 @@
 
     Write-LogInfo 'Applying Windows personalisation configuration...'
 
+    Set-WinHomeLocation -GeoId 140
+
     Set-Variable -Option Constant LanguageList (Get-WinUserLanguageList)
     if (-not ($LanguageList | Where-Object LanguageTag -Like 'lv')) {
         $LanguageList.Add('lv-LV')
@@ -14,12 +16,6 @@
     [String]$ConfigLines = ''
 
     try {
-        foreach ($Registry in (Get-UsersRegistryKeys)) {
-            # [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Creative\$User]
-            # `"RotatingLockScreenEnabled`"=dword:00000001
-            # `"RotatingLockScreenOverlayEnabled`"=dword:00000001`n"
-        }
-
         Set-Variable -Option Constant NotificationRegistries ((Get-Item 'HKCU:\Control Panel\NotifyIconSettings\*').Name)
         foreach ($Registry in $NotificationRegistries) {
             $ConfigLines += "`n[$Registry]`n"
