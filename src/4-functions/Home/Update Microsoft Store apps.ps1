99998077d2e2d99f093969c4b483af5fe94cff38
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2025-10-02 21:45:46 +0300

    Update Microsoft Store apps asynchronously in a separate process

--- src/4-functions/Home/Update Microsoft Store apps.ps1
+++ src/4-functions/Home/Update Microsoft Store apps.ps1
@@ -2,11 +2,7 @@ function Update-MicrosoftStoreApps {
     Write-LogInfo 'Starting Microsoft Store apps update...'
 
     try {
-        Set-Variable -Option Constant Status (Get-CimInstance MDM_EnterpriseModernAppManagement_AppManagement01 -Namespace 'root\cimv2\mdm\dmmap' | Invoke-CimMethod -MethodName 'UpdateScanMethod').ReturnValue
-
-        if ($Status -ne 0) {
-            Write-LogError 'Failed to update Microsoft Store apps'
-        }
+        Invoke-CustomCommand -Elevated -HideWindow "Get-CimInstance MDM_EnterpriseModernAppManagement_AppManagement01 -Namespace 'root\cimv2\mdm\dmmap' | Invoke-CimMethod -MethodName 'UpdateScanMethod'"    
     } catch [Exception] {
         Write-ExceptionLog $_ 'Failed to update Microsoft Store apps'
         return
