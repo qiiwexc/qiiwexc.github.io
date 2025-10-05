1e6c9e8d2a759550b8b0e15842a1509c572072c8
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2025-10-05 17:26:04 +0300

    Check for Internet connection before loading SophiaScript

--- src/4-functions/Configuration/Windows/Set-FileAssociations.ps1
+++ src/4-functions/Configuration/Windows/Set-FileAssociations.ps1
@@ -4,11 +4,14 @@ function Set-FileAssociations {
     Set-Variable -Option Constant SophiaScriptUrl "https://raw.githubusercontent.com/farag2/Sophia-Script-for-Windows/master/src/Sophia_Script_for_Windows_$OS_VERSION/Module/Sophia.psm1"
     Set-Variable -Option Constant SophiaScriptPath "$PATH_TEMP_DIR\Sophia.ps1"
 
-    Start-BitsTransfer -Source $SophiaScriptUrl -Destination $SophiaScriptPath -Dynamic
+    Set-Variable -Option Constant NoConnection (Test-NetworkConnection)
+    if (-not $NoConnection) {
+        Start-BitsTransfer -Source $SophiaScriptUrl -Destination $SophiaScriptPath -Dynamic
 
-    (Get-Content -Path $SophiaScriptPath -Force) | Set-Content -Path $SophiaScriptPath -Encoding UTF8 -Force
+        (Get-Content -Path $SophiaScriptPath -Force) | Set-Content -Path $SophiaScriptPath -Encoding UTF8 -Force
 
-    . $SophiaScriptPath
+        . $SophiaScriptPath
+    }
 
     foreach ($FileAssociation in $CONFIG_FILE_ASSOCIATIONS) {
         [String]$Extension = $FileAssociation.Extension
@@ -23,7 +26,7 @@ function Set-FileAssociations {
             & cmd.exe /c assoc $Extension=$Application
         }
 
-        if ($FileAssociation.Method -eq 'Sophia') {
+        if (-not $NoConnection -and $FileAssociation.Method -eq 'Sophia') {
             Set-Association -ProgramPath $Application -Extension $Extension
         }
     }
