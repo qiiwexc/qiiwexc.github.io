7c1db934489847bfbfc686e7e7fb2bc62b7b6790
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2025-10-11 02:20:56 +0300

    Improve performance

--- src/4-functions/Configuration/Windows/Set-WindowsBaseConfiguration.ps1
+++ src/4-functions/Configuration/Windows/Set-WindowsBaseConfiguration.ps1
@@ -8,21 +8,7 @@
     Write-ActivityProgress -PercentComplete 5 -Task 'Applying Windows configuration...'
 
     if ($PS_VERSION -ge 5) {
-        Set-MpPreference -CheckForSignaturesBefore $True
-        Set-MpPreference -DisableBlockAtFirstSeen $False
-        Set-MpPreference -DisableCatchupQuickScan $False
-        Set-MpPreference -DisableEmailScanning $False
-        Set-MpPreference -DisableRemovableDriveScanning $False
-        Set-MpPreference -DisableRestorePoint $False
-        Set-MpPreference -DisableScanningMappedNetworkDrivesForFullScan $False
-        Set-MpPreference -DisableScanningNetworkFiles $False
-        Set-MpPreference -EnableFileHashComputation $True
-        Set-MpPreference -EnableNetworkProtection Enabled
-        Set-MpPreference -PUAProtection Enabled
-        Set-MpPreference -AllowSwitchToAsyncInspection $True
-        Set-MpPreference -MeteredConnectionUpdates $True
-        Set-MpPreference -IntelTDTEnabled $True
-        Set-MpPreference -BruteForceProtectionLocalNetworkBlocking $True
+        Set-WindowsSecurityConfiguration
     }
 
     Set-ItemProperty -Path 'HKCU:\Control Panel\International' -Name 'sCurrency' -Value ([Char]0x20AC)
@@ -30,45 +16,45 @@
 
     Set-Variable -Option Constant UnelevatedExplorerTaskName 'CreateExplorerShellUnelevatedTask'
     if (Get-ScheduledTask | Where-Object { $_.TaskName -eq $UnelevatedExplorerTaskName } ) {
-        Unregister-ScheduledTask -TaskName $UnelevatedExplorerTaskName -Confirm $False
+        Unregister-ScheduledTask -TaskName $UnelevatedExplorerTaskName -Confirm:$False
     }
 
     Set-Variable -Option Constant LocalisedConfig $(if ($SYSTEM_LANGUAGE -match 'ru') { $CONFIG_WINDOWS_RUSSIAN } else { $CONFIG_WINDOWS_ENGLISH })
 
-    [String]$ConfigLines = $CONFIG_WINDOWS_HKEY_CURRENT_USER.Replace('HKEY_CURRENT_USER', 'HKEY_USERS\.DEFAULT')
-    $ConfigLines += "`n"
-    $ConfigLines += $CONFIG_WINDOWS_HKEY_CLASSES_ROOT
-    $ConfigLines += "`n"
-    $ConfigLines += $CONFIG_WINDOWS_HKEY_CURRENT_USER
-    $ConfigLines += "`n"
-    $ConfigLines += $CONFIG_WINDOWS_HKEY_LOCAL_MACHINE
-    $ConfigLines += "`n"
-    $ConfigLines += $LocalisedConfig.Replace('HKEY_CURRENT_USER', 'HKEY_USERS\.DEFAULT')
-    $ConfigLines += "`n"
-    $ConfigLines += $LocalisedConfig
+    [Collections.Generic.List[String]]$ConfigLines = $CONFIG_WINDOWS_HKEY_CURRENT_USER.Replace('HKEY_CURRENT_USER', 'HKEY_USERS\.DEFAULT')
+    $ConfigLines.Add("`n")
+    $ConfigLines.Add($CONFIG_WINDOWS_HKEY_CLASSES_ROOT)
+    $ConfigLines.Add("`n")
+    $ConfigLines.Add($CONFIG_WINDOWS_HKEY_CURRENT_USER)
+    $ConfigLines.Add("`n")
+    $ConfigLines.Add($CONFIG_WINDOWS_HKEY_LOCAL_MACHINE)
+    $ConfigLines.Add("`n")
+    $ConfigLines.Add($LocalisedConfig.Replace('HKEY_CURRENT_USER', 'HKEY_USERS\.DEFAULT'))
+    $ConfigLines.Add("`n")
+    $ConfigLines.Add($LocalisedConfig)
 
     Write-ActivityProgress -PercentComplete 10
 
     try {
         foreach ($Registry in (Get-UsersRegistryKeys)) {
             [String]$User = $Registry.Replace('HKEY_USERS\', '')
-            $ConfigLines += "`n[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Creative\$User]`n"
-            $ConfigLines += "`"RotatingLockScreenOverlayEnabled`"=dword:00000000`n"
+            $ConfigLines.Add("`n[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Creative\$User]`n")
+            $ConfigLines.Add("`"RotatingLockScreenOverlayEnabled`"=dword:00000000`n")
 
-            $ConfigLines += "`n[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\InstallService\Stubification\$User]`n"
-            $ConfigLines += "`"EnableAppOffloading`"=dword:00000000`n"
+            $ConfigLines.Add("`n[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\InstallService\Stubification\$User]`n")
+            $ConfigLines.Add("`"EnableAppOffloading`"=dword:00000000`n")
 
-            $ConfigLines += "`n[HKEY_USERS\$($User)_Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\Main]`n"
-            $ConfigLines += "`"DoNotTrack`"=dword:00000001`n"
+            $ConfigLines.Add("`n[HKEY_USERS\$($User)_Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\Main]`n")
+            $ConfigLines.Add("`"DoNotTrack`"=dword:00000001`n")
 
-            $ConfigLines += "`n[HKEY_USERS\$($User)_Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\ServiceUI]`n"
-            $ConfigLines += "`"EnableCortana`"=dword:00000000`n"
+            $ConfigLines.Add("`n[HKEY_USERS\$($User)_Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\ServiceUI]`n")
+            $ConfigLines.Add("`"EnableCortana`"=dword:00000000`n")
         }
 
         Set-Variable -Option Constant VolumeRegistries ((Get-Item 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\BitBucket\Volume\*').Name)
         foreach ($Registry in $VolumeRegistries) {
-            $ConfigLines += "`n[$Registry]`n"
-            $ConfigLines += "`"MaxCapacity`"=dword:000FFFFF`n"
+            $ConfigLines.Add("`n[$Registry]`n")
+            $ConfigLines.Add("`"MaxCapacity`"=dword:000FFFFF`n")
         }
     } catch [Exception] {
         Write-LogException $_ 'Failed to read the registry' $LogIndentLevel
--- utils/build-batch-script.ps1
+++ utils/build-batch-script.ps1
@@ -6,27 +6,27 @@ function New-BatchScript {
 
     Write-LogInfo 'Building batch script...'
 
-    [String[]]$PowerShellStrings = Get-Content $Ps1File
+    [Collections.Generic.List[String]]$PowerShellStrings = Get-Content $Ps1File
 
-    [String[]]$BatchStrings = "@echo off`n"
-    $BatchStrings += "if `"%~1`"==`"Debug`" set debug=true`n"
-    $BatchStrings += "set `"psfile=%temp%\$ProjectName.ps1`"`n"
-    $BatchStrings += '> "%psfile%" ('
-    $BatchStrings += "  for /f `"delims=`" %%A in ('findstr `"^::`" `"%~f0`"') do ("
-    $BatchStrings += '    set "line=%%A"'
-    $BatchStrings += '    setlocal enabledelayedexpansion'
-    $BatchStrings += '    echo(!line:~2!'
-    $BatchStrings += '    endlocal'
-    $BatchStrings += '  )'
-    $BatchStrings += ")`n"
-    $BatchStrings += 'if "%debug%"=="true" ('
-    $BatchStrings += "  powershell -ExecutionPolicy Bypass -Command `"& '%psfile%' -WorkingDirectory '%cd%' -DevMode`""
-    $BatchStrings += ') else ('
-    $BatchStrings += "  powershell -ExecutionPolicy Bypass -Command `"& '%psfile%' -WorkingDirectory '%cd%'`""
-    $BatchStrings += ")`n"
+    [Collections.Generic.List[String]]$BatchStrings = "@echo off`n"
+    $BatchStrings.Add("if `"%~1`"==`"Debug`" set debug=true`n")
+    $BatchStrings.Add("set `"psfile=%temp%\$ProjectName.ps1`"`n")
+    $BatchStrings.Add('> "%psfile%" (')
+    $BatchStrings.Add("  for /f `"delims=`" %%A in ('findstr `"^::`" `"%~f0`"') do (")
+    $BatchStrings.Add('    set "line=%%A"')
+    $BatchStrings.Add('    setlocal enabledelayedexpansion')
+    $BatchStrings.Add('    echo(!line:~2!')
+    $BatchStrings.Add('    endlocal')
+    $BatchStrings.Add('  )')
+    $BatchStrings.Add(")`n")
+    $BatchStrings.Add('if "%debug%"=="true" (')
+    $BatchStrings.Add("  powershell -ExecutionPolicy Bypass -Command `"& '%psfile%' -WorkingDirectory '%cd%' -DevMode`"")
+    $BatchStrings.Add(') else (')
+    $BatchStrings.Add("  powershell -ExecutionPolicy Bypass -Command `"& '%psfile%' -WorkingDirectory '%cd%'`"")
+    $BatchStrings.Add(")`n")
 
     foreach ($String in $PowerShellStrings) {
-        $BatchStrings += "::$($String.Replace("`n", "`n::"))"
+        $BatchStrings.Add("::$($String.Replace("`n", "`n::"))")
     }
 
     Write-LogInfo "Writing batch file $BatchFile"
