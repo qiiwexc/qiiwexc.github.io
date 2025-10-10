d7e7f224af7b07743b5ee7f674a6e1a147cc6102
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2025-10-10 23:44:54 +0300

    Expand Windows feature cleanup

--- src/2-ui/3-Configuration/4 Remove Windows components.ps1
+++ src/2-ui/3-Configuration/4 Remove Windows components.ps1
@@ -1,6 +1,5 @@
 New-GroupBox 'Remove Windows components'
 
 
-[Switch]$BUTTON_DISABLED = $PS_VERSION -lt 5
 [ScriptBlock]$BUTTON_FUNCTION = { Remove-WindowsFeatures }
-New-Button 'Feature cleanup' $BUTTON_FUNCTION -Disabled:$BUTTON_DISABLED
+New-Button 'Feature cleanup' $BUTTON_FUNCTION
--- src/4-functions/Configuration/Windows/Remove-WindowsFeatures.ps1
+++ src/4-functions/Configuration/Windows/Remove-WindowsFeatures.ps1
@@ -1,40 +1,62 @@
 function Remove-WindowsFeatures {
+    Set-Variable -Option Constant LogIndentLevel 1
+
     New-Activity 'Removing miscellaneous Windows features...'
 
-    Set-Variable -Option Constant FeaturesToRemove @('App.StepsRecorder',
-        'App.Support.QuickAssist',
-        'MathRecognizer',
-        'Media.WindowsMediaPlayer',
-        'Microsoft.Windows.WordPad',
-        'OpenSSH.Client'
-    )
-
-    try {
-        Write-ActivityProgress -PercentComplete 5 -Task 'Collecting the features to remove...'
-        Set-Variable -Option Constant InstalledCapabilities (Get-WindowsCapability -Online | Where-Object { $_.State -eq 'Installed' })
-        Set-Variable -Option Constant CapabilitiesToRemove ($InstalledCapabilities | Where-Object { $_.Name.Split('~')[0] -in $FeaturesToRemove })
-    } catch [Exception] {
-        Write-ExceptionLog $_ 'Failed to collect the features to remove'
+    if ($PS_VERSION -ge 5) {
+        try {
+            Write-ActivityProgress -PercentComplete 5 -Task 'Collecting capabilities to remove...'
+            Set-Variable -Option Constant InstalledCapabilities (Get-WindowsCapability -Online | Where-Object { $_.State -eq 'Installed' })
+            Set-Variable -Option Constant CapabilitiesToRemove ($InstalledCapabilities | Where-Object { $_.Name.Split('~')[0] -in $CONFIG_CAPABILITIES_TO_REMOVE })
+            Set-Variable -Option Constant CapabilityCount ($CapabilitiesToRemove.Count)
+            Out-Success $LogIndentLevel
+        } catch [Exception] {
+            Write-LogException $_ 'Failed to collect capabilities to remove' $LogIndentLevel
+        }
     }
 
-    Set-Variable -Option Constant CapabilityCount ($CapabilitiesToRemove.Count)
+    try {
+        Write-ActivityProgress -PercentComplete 10 -Task 'Collecting features to remove...'
+        Set-Variable -Option Constant InstalledFeatures (Get-WindowsOptionalFeature -Online | Where-Object { $_.State -eq 'Enabled' })
+        Set-Variable -Option Constant FeaturesToRemove ($InstalledFeatures | Where-Object { $_.FeatureName -in $CONFIG_FEATURES_TO_REMOVE })
+        Set-Variable -Option Constant FeatureCount ($FeaturesToRemove.Count)
+        Out-Success $LogIndentLevel
+    } catch [Exception] {
+        Write-LogException $_ 'Failed to collect features to remove' $LogIndentLevel
+    }
 
-    if ($CapabilityCount -eq 0) {
-        Write-LogInfo 'Nothing to remove'
-    } else {
-        Set-Variable -Option Constant Step ([Math]::Floor(80 / $CapabilityCount))
+    if ($CapabilityCount) {
+        Set-Variable -Option Constant CapabilityStep ([Math]::Floor(50 / $CapabilityCount))
 
         [Int]$Iteration = 1
         foreach ($Capability in $CapabilitiesToRemove) {
             [String]$Name = $Capability.Name
             try {
-                [Int]$Percentage = 10 + $Iteration * $Step
+                [Int]$Percentage = 15 + $Iteration * $CapabilityStep
                 Write-ActivityProgress -PercentComplete $Percentage -Task "Removing '$Name'..."
                 $Iteration++
                 Remove-WindowsCapability -Online -Name "$Name"
-                Out-Success
+                Out-Success $LogIndentLevel
             } catch [Exception] {
-                Write-ExceptionLog $_ "Failed to remove '$Name'"
+                Write-LogException $_ "Failed to remove '$Name'" $LogIndentLevel
+            }
+        }
+    }
+
+    if ($FeatureCount) {
+        Set-Variable -Option Constant FeatureStep ([Math]::Floor(20 / $FeatureCount))
+
+        [Int]$Iteration = 1
+        foreach ($Feature in $FeaturesToRemove) {
+            [String]$Name = $Feature.FeatureName
+            try {
+                [Int]$Percentage = 70 + $Iteration * $FeatureStep
+                Write-ActivityProgress -PercentComplete $Percentage -Task "Removing '$Name'..."
+                $Iteration++
+                Disable-WindowsOptionalFeature -Online -Remove -NoRestart -FeatureName "$Name"
+                Out-Success $LogIndentLevel
+            } catch [Exception] {
+                Write-LogException $_ "Failed to remove '$Name'" $LogIndentLevel
             }
         }
     }
@@ -42,7 +64,7 @@ function Remove-WindowsFeatures {
     if (Test-Path 'mstsc.exe') {
         Write-ActivityProgress -PercentComplete 90 -Task "Removing 'mstsc'..."
         Start-Process 'mstsc' '/uninstall'
-        Out-Success
+        Out-Success $LogIndentLevel
     }
 
     Write-ActivityCompleted
