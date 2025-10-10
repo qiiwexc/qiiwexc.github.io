34e2d7e653a3fd8fcb81447ed87ab902733de8ef
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2025-10-10 23:44:54 +0300

    Expand Windows feature cleanup

---
+++ src/3-configs/Windows/Capabilities to remove.ps1
@@ -0,0 +1,12 @@
+Set-Variable -Option Constant CONFIG_CAPABILITIES_TO_REMOVE @(
+    'App.StepsRecorder'
+    'App.Support.QuickAssist'
+    'Language.Handwriting'
+    'Language.Speech'
+    'Language.TextToSpeech'
+    'MathRecognizer'
+    'Media.WindowsMediaPlayer'
+    'Microsoft.Windows.WordPad'
+    'OneCoreUAP.OneSync'
+    'OpenSSH.Client'
+)
---
+++ src/3-configs/Windows/Features to remove.ps1
@@ -0,0 +1,6 @@
+Set-Variable -Option Constant CONFIG_FEATURES_TO_REMOVE @(
+    'MediaPlayback'
+    'Microsoft-RemoteDesktopConnection'
+    'MicrosoftWindowsPowerShellV2Root'
+    'Recall'
+)
