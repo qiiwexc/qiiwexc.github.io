b432a954c0c57c8c8e9b7dcafff650b4cc8ea8d1
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2025-11-25 11:49:53 +0200

    Minor updates to the logger

--- utils/common/logger.ps1
+++ utils/common/logger.ps1
@@ -1,48 +1,36 @@
 function Write-LogInfo {
     param(
-        [String][Parameter(Position = 0, Mandatory)]$Message
+        [String][Parameter(Position = 0, Mandatory)]$Message,
+        [Int][Parameter(Position = 1)]$Level = 0
     )
-    Write-Log 'INFO' $Message
+    Write-Log 'INFO' $Message -IndentLevel $Level
 }
 
 function Write-LogWarning {
     param(
-        [String][Parameter(Position = 0, Mandatory)]$Message
+        [String][Parameter(Position = 0, Mandatory)]$Message,
+        [Int][Parameter(Position = 1)]$Level = 0
     )
-    Write-Log 'WARN' "$(Get-Emoji '26A0') $Message"
+    Write-Log 'WARN' "$(Get-Emoji '26A0') $Message" -IndentLevel $Level
 }
 
 function Write-LogError {
     param(
-        [String][Parameter(Position = 0, Mandatory)]$Message
+        [String][Parameter(Position = 0, Mandatory)]$Message,
+        [Int][Parameter(Position = 1)]$Level = 0
     )
-    Write-Log 'ERROR' "$(Get-Emoji '274C') $Message"
-}
-
-function Out-Success {
-    Write-LogInfo "   > Done $(Get-Emoji '2705')"
-}
-
-function Out-Failure {
-    Write-LogInfo "   > Failed $(Get-Emoji '274C')"
-}
-
-function Write-LogException {
-    param(
-        [Object][Parameter(Position = 0, Mandatory)]$Exception,
-        [String][Parameter(Position = 1, Mandatory)]$Message
-    )
-
-    Write-LogError "$($Message): $($Exception.Exception.Message)"
+    Write-Log 'ERROR' "$(Get-Emoji '274C') $Message" -IndentLevel $Level
 }
 
 function Write-Log {
     param(
         [String][Parameter(Position = 0, Mandatory)][ValidateSet('INFO', 'WARN', 'ERROR')]$Level,
-        [String][Parameter(Position = 1, Mandatory)]$Message
+        [String][Parameter(Position = 1, Mandatory)]$Message,
+        [Int][Parameter(Position = 2)]$IndentLevel = 0
     )
 
-    Set-Variable -Option Constant Text "[$((Get-Date).ToString())] $Message"
+    Set-Variable -Option Constant Indent $('   ' * $IndentLevel)
+    Set-Variable -Option Constant Text "[$((Get-Date).ToString())]$Indent $Message"
 
     switch ($Level) {
         'INFO' {
@@ -58,6 +46,44 @@ function Write-Log {
             Write-Host $Text
         }
     }
+}
+
+
+function Out-Status {
+    param(
+        [String][Parameter(Position = 0, Mandatory)]$Status,
+        [Int][Parameter(Position = 1)]$Level = 0
+    )
+
+    Write-LogInfo "   > $Status" $Level
+}
+
+
+function Out-Success {
+    param(
+        [Int][Parameter(Position = 0)]$Level = 0
+    )
+
+    Out-Status "Done $(Get-Emoji '2705')" $Level
+}
+
+function Out-Failure {
+    param(
+        [Int][Parameter(Position = 0)]$Level = 0
+    )
+
+    Out-Status "Failed $(Get-Emoji '274C')" $Level
+}
+
+
+function Write-LogException {
+    param(
+        [Object][Parameter(Position = 0, Mandatory)]$Exception,
+        [String][Parameter(Position = 1, Mandatory)]$Message,
+        [Int][Parameter(Position = 2)]$Level = 0
+    )
+
+    Write-Log 'ERROR' "$($Message): $($Exception.Exception.Message)" -IndentLevel $Level
 }
 
 function Get-Emoji {
