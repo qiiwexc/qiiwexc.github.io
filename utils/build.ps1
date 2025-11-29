f6803665ec21ee5b2dc7e2e5e2f1a05e61c81451
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2025-11-29 03:22:06 +0200

    Improve builder

--- utils/build.ps1
+++ utils/build.ps1
@@ -2,8 +2,30 @@
 #Requires -Version 3
 
 param(
-    [Switch]$Run
+    [Switch]$Autounattend,
+    [Switch]$Bat,
+    [Switch]$Dev,
+    [Switch]$Full,
+    [Switch]$Html,
+    [Switch]$Ps1,
+    [Switch]$Update
 )
+
+if ($Full) {
+    $Autounattend = $true
+    $Bat = $true
+    $Html = $true
+    $Ps1 = $true
+    $Update = $true
+}
+
+if ($Dev) {
+    $Bat = $true
+}
+
+if ($Bat) {
+    $Ps1 = $true
+}
 
 Set-Variable -Option Constant Version ([String](Get-Date -Format 'y.M.d'))
 Set-Variable -Option Constant ProjectName ([String]'qiiwexc')
@@ -15,21 +37,21 @@ Set-Variable -Option Constant SourcePath ([String]'.\src')
 Set-Variable -Option Constant DistPath ([String]'.\d')
 Set-Variable -Option Constant BuilderPath ([String]"$UtilsPath\build")
 Set-Variable -Option Constant CommonPath ([String]"$UtilsPath\common")
-Set-Variable -Option Constant UnattendedPath ([String]"$BuilderPath\unattended")
+
+Set-Variable -Option Constant VersionFile ([String]"$DistPath\version")
+Set-Variable -Option Constant Ps1File ([String]"$DistPath\$ProjectName.ps1")
+Set-Variable -Option Constant BatchFile ([String]"$DistPath\$ProjectName.bat")
+Set-Variable -Option Constant UnattendedFile ([String]"$DistPath\autounattend-{LOCALE}.xml")
 
 . "$CommonPath\logger.ps1"
 . "$BuilderPath\Get-Config.ps1"
 . "$BuilderPath\New-BatchScript.ps1"
 . "$BuilderPath\New-HtmlFile.ps1"
 . "$BuilderPath\New-PowerShellScript.ps1"
+. "$BuilderPath\New-UnattendedFile.ps1"
 . "$BuilderPath\Set-Urls.ps1"
+. "$BuilderPath\Update-Dependencies.ps1"
 . "$BuilderPath\Write-VersionFile.ps1"
-. "$UnattendedPath\New-UnattendedFile.ps1"
-
-Set-Variable -Option Constant VersionFile ([String]"$DistPath\version")
-Set-Variable -Option Constant Ps1File ([String]"$DistPath\$ProjectName.ps1")
-Set-Variable -Option Constant BatchFile ([String]"$DistPath\$ProjectName.bat")
-Set-Variable -Option Constant UnattendedFile ([String]"$DistPath\autounattend-{LOCALE}.xml")
 
 Write-LogInfo 'Build task started'
 Write-LogInfo "Version     = $Version"
@@ -38,30 +60,45 @@ Write-LogInfo "Output file = $Ps1File"
 
 Write-Progress -Activity 'Build' -PercentComplete 1
 
-Set-Urls $ConfigPath $TemplatesPath
-Write-Progress -Activity 'Build' -PercentComplete 10
+if ($Update) {
+    Update-Dependencies $ConfigPath $BuilderPath
+    Write-Progress -Activity 'Build' -PercentComplete 30
+}
 
-Set-Variable -Option Constant Config (Get-Config $ConfigPath $Version)
-Write-Progress -Activity 'Build' -PercentComplete 20
+if ($Html -or $Ps1) {
+    Set-Urls $ConfigPath $TemplatesPath
+    Write-Progress -Activity 'Build' -PercentComplete 40
 
-Write-VersionFile $Version $VersionFile
-Write-Progress -Activity 'Build' -PercentComplete 30
+    Set-Variable -Option Constant Config (Get-Config $ConfigPath $Version)
+    Write-Progress -Activity 'Build' -PercentComplete 50
 
-New-HtmlFile $TemplatesPath $Config
-Write-Progress -Activity 'Build' -PercentComplete 50
+    Write-VersionFile $Version $VersionFile
+    Write-Progress -Activity 'Build' -PercentComplete 60
+}
 
-New-UnattendedFile $UnattendedPath $SourcePath $TemplatesPath $UnattendedFile
-Write-Progress -Activity 'Build' -PercentComplete 60
+if ($Html) {
+    New-HtmlFile $TemplatesPath $Config
+    Write-Progress -Activity 'Build' -PercentComplete 70
+}
 
-New-PowerShellScript $SourcePath $Ps1File -Config $Config
-Write-Progress -Activity 'Build' -PercentComplete 80
+if ($Autounattend) {
+    New-UnattendedFile $ConfigPath $BuilderPath $SourcePath $TemplatesPath $UnattendedFile
+    Write-Progress -Activity 'Build' -PercentComplete 80
+}
 
-New-BatchScript $Ps1File $BatchFile
+if ($Ps1) {
+    New-PowerShellScript $SourcePath $Ps1File -Config $Config
+    Write-Progress -Activity 'Build' -PercentComplete 90
+}
+
+if ($Bat) {
+    New-BatchScript $Ps1File $BatchFile
+}
+
 Write-Progress -Activity 'Build' -Complete
-
 Write-LogInfo 'Build finished'
 
-if ($Run) {
+if ($Dev) {
     Write-LogInfo "Running $BatchFile"
     Start-Process 'PowerShell' ".\$BatchFile Debug"
 }
--- utils/build/New-UnattendedFile.ps1
+++ utils/build/New-UnattendedFile.ps1
@@ -1,11 +1,22 @@
 function New-UnattendedFile {
     param(
-        [String][Parameter(Position = 0, Mandatory)]$UnattendedPath,
-        [String][Parameter(Position = 1, Mandatory)]$SourcePath,
-        [String][Parameter(Position = 2, Mandatory)]$TemplatesPath,
-        [String][Parameter(Position = 3, Mandatory)]$FileNameTemplate
+        [String][Parameter(Position = 0, Mandatory)]$ConfigPath,
+        [String][Parameter(Position = 1, Mandatory)]$BuilderPath,
+        [String][Parameter(Position = 2, Mandatory)]$SourcePath,
+        [String][Parameter(Position = 3, Mandatory)]$TemplatesPath,
+        [String][Parameter(Position = 4, Mandatory)]$FileNameTemplate
     )
 
+    Write-LogInfo 'Building unattended files...'
+
+    Set-Variable -Option Constant UnattendedPath ([String]"$BuilderPath\unattended")
+    Set-Variable -Option Constant ConfigsPath ([String]"$SourcePath\3-configs")
+
+    Set-Variable -Option Constant TemplateFile ([String]"$TemplatesPath\autounattend.xml")
+
+    Set-Variable -Option Constant Locales ([Collections.Generic.List[String]]@('English', 'Russian'))
+
+    . "$UnattendedPath\New-UnattendedTemplate.ps1"
     . "$UnattendedPath\Set-AppRemovalList.ps1"
     . "$UnattendedPath\Set-CapabilitiesRemovalList.ps1"
     . "$UnattendedPath\Set-FeaturesRemovalList.ps1"
@@ -14,12 +25,7 @@ function New-UnattendedFile {
     . "$UnattendedPath\Set-PowerSchemeConfiguration.ps1"
     . "$UnattendedPath\Set-WindowsSecurityConfiguration.ps1"
 
-    Write-LogInfo 'Building unattended files...'
-
-    Set-Variable -Option Constant ConfigsPath ([String]"$SourcePath\3-configs")
-    Set-Variable -Option Constant TemplateFile ([String]"$TemplatesPath\autounattend.xml")
-
-    Set-Variable -Option Constant Locales ([Collections.Generic.List[String]]@('English', 'Russian'))
+    New-UnattendedTemplate $ConfigPath $TemplateFile
 
     foreach ($Locale in $Locales) {
         [String]$OutputFileName = $FileNameTemplate.Replace('{LOCALE}', $Locale)
