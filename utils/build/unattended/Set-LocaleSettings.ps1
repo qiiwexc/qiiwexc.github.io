cc1657d92a5a46d81460b5d2fa2d0494183cb5c6
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2025-12-06 00:57:12 +0200

    Fix duplicate English keyboard on Russian systems

--- utils/build/unattended/Set-LocaleSettings.ps1
+++ utils/build/unattended/Set-LocaleSettings.ps1
@@ -13,7 +13,7 @@ function Set-LocaleSettings {
 }
 
 Set-Variable -Option Constant LOCALE_PARAMETERS_ENGLISH (
-    [Collections.Generic.List[hashtable]]@(
+    [Collections.Generic.List[Hashtable]]@(
         @{Key = 'KEYBOARD1'; Value = '0809' },
         @{Key = 'KEYBOARD2'; Value = '0426' },
         @{Key = 'KEYBOARD3'; Value = '0419' },
@@ -26,20 +26,20 @@ Set-Variable -Option Constant LOCALE_PARAMETERS_ENGLISH (
 )
 
 Set-Variable -Option Constant LOCALE_PARAMETERS_RUSSIAN (
-    [Collections.Generic.List[hashtable]]@(
+    [Collections.Generic.List[Hashtable]]@(
         @{Key = 'KEYBOARD1'; Value = '0419' }
         @{Key = 'KEYBOARD2'; Value = '0426' },
-        @{Key = 'KEYBOARD3'; Value = '0809' },
+        @{Key = 'KEYBOARD3'; Value = '0409' },
         @{Key = 'LOCALE1'; Value = 'ru-RU' },
         @{Key = 'LOCALE2'; Value = 'lv-LV' },
-        @{Key = 'LOCALE3'; Value = 'en-GB' },
+        @{Key = 'LOCALE3'; Value = 'en-US' },
         @{Key = 'UI_LANGUAGE_FALLBACK'; Value = 'en-US' },
         @{Key = 'UI_LANGUAGE'; Value = 'ru-RU' }
     )
 )
 
 Set-Variable -Option Constant LOCALE_PARAMETERS (
-    [hashtable]@{
+    [Hashtable]@{
         English = $LOCALE_PARAMETERS_ENGLISH
         Russian = $LOCALE_PARAMETERS_RUSSIAN
     }
