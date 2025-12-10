8bf78e93410e08a13103f38d990832e18f3f42e9
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2025-12-11 01:09:39 +0200

    `UseBasicParsing` with `Invoke-WebRequest`

--- utils/build/updates/Update-WebDependency.ps1
+++ utils/build/updates/Update-WebDependency.ps1
@@ -7,7 +7,7 @@ function Update-WebDependency {
 
     Write-LogInfo "Fetching URL: $Uri" 1
 
-    Set-Variable -Option Constant Response ([Object](Invoke-WebRequest $Uri))
+    Set-Variable -Option Constant Response ([Object](Invoke-WebRequest -UseBasicParsing -Uri $Uri))
 
     if (-not $Response.Content) {
         Write-LogError "Failed to fetch URL: $Uri" 1
