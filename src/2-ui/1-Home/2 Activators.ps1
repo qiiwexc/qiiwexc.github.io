bcbe6a4d12fb2e3ba7c02bfac9a3981f5689588b
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2026-01-01 20:32:03 +0200

    Remove ActivationProgram

--- src/2-ui/1-Home/2 Activators.ps1
+++ src/2-ui/1-Home/2 Activators.ps1
@@ -1,12 +1,6 @@
-New-GroupBox 'Activators (Windows 7+, Office)'
+New-GroupBox 'Activators'
 
 
 [Switch]$BUTTON_DISABLED = $OS_VERSION -lt 7
 [ScriptBlock]$BUTTON_FUNCTION = { Start-Activator }
 New-Button 'MAS Activator' $BUTTON_FUNCTION -Disabled:$BUTTON_DISABLED
-
-
-[ScriptBlock]$BUTTON_FUNCTION = { Start-DownloadUnzipAndRun '{URL_ACTIVATION_PROGRAM}' -AVWarning -Execute:$CHECKBOX_StartActivationProgram.Checked }
-New-Button 'Activation Program' $BUTTON_FUNCTION
-
-[Windows.Forms.CheckBox]$CHECKBOX_StartActivationProgram = New-CheckBoxRunAfterDownload -Checked
