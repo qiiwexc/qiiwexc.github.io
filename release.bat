03a2dbe981a2d6ec8d677698f800753f36ff439a
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2026-01-04 00:58:26 +0200

    Add release script

---
+++ release.bat
@@ -0,0 +1,11 @@
+set /p version=< d/version
+echo %version%
+
+git fetch --all --prune
+git pull --tags --autostash -r origin master
+git add -A -- d
+git add -A -- index.html
+git commit -m "Release %version%"
+git tag -a v%version% -m "Release %version%"
+git push origin master:master
+git push origin --tags
