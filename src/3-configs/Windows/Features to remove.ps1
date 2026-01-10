ffc276d4c0ec336e240c1ca853eafb8d9e916701
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2026-01-10 13:40:00 +0100

    Drop some configuration

--- src/3-configs/Windows/Features to remove.ps1
+++ src/3-configs/Windows/Features to remove.ps1
@@ -1,7 +1,6 @@
 Set-Variable -Option Constant CONFIG_FEATURES_TO_REMOVE (
     [Collections.Generic.List[String]]@(
         'MediaPlayback'
-        'Microsoft-RemoteDesktopConnection'
         'MicrosoftWindowsPowerShellV2Root'
         'Recall'
     )
