c374e79318cb59693ba4b8b9cd57be018f977435
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2026-01-13 01:20:11 +0200

    Drop some configuration

--- src/3-configs/Windows/Features to remove.ps1
+++ src/3-configs/Windows/Features to remove.ps1
@@ -1,6 +1,5 @@
 Set-Variable -Option Constant CONFIG_FEATURES_TO_REMOVE (
     [Collections.Generic.List[String]]@(
-        'MediaPlayback'
         'MicrosoftWindowsPowerShellV2Root'
         'Recall'
     )
