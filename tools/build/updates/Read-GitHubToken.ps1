77fc9828b5e78ca69335db5021d179674060925b
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2026-01-27 18:41:07 +0200

    Handle missing env file

--- tools/build/updates/Read-GitHubToken.ps1
+++ tools/build/updates/Read-GitHubToken.ps1
@@ -5,6 +5,11 @@ function Read-GitHubToken {
 
     Write-LogInfo 'Reading GitHub token'
 
+    if (-not (Test-Path $EnvPath)) {
+        Write-LogWarning "Environment file not found at path: $EnvPath"
+        return
+    }
+
     Set-Variable -Option Constant EnvContent ([String[]](Read-TextFile -AsList $EnvPath))
     Set-Variable -Option Constant Lines ([String[]]($EnvContent | ForEach-Object { $_.Trim() }))
 
@@ -20,5 +25,5 @@ function Read-GitHubToken {
         }
     }
 
-    return $Null
+    return
 }
