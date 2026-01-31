ae8af057d477abcc4c7d3702065621b05c615455
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2026-02-01 00:01:33 +0200

    Update unattended template

--- src/2-ui/2-Installs/1 Ninite.ps1
+++ src/2-ui/2-Installs/1 Ninite.ps1
@@ -4,25 +4,12 @@ New-GroupBox 'Ninite'
 
 
 [Windows.Forms.CheckBox]$CHECKBOX_Ninite_Chrome = New-CheckBox 'Google Chrome' -Name 'chrome' -Checked
-$CHECKBOX_Ninite_Chrome.Add_CheckStateChanged( { Set-NiniteButtonState } )
-
 [Windows.Forms.CheckBox]$CHECKBOX_Ninite_Firefox = New-CheckBox 'Mozilla Firefox' -Name 'firefox' -Checked
-$CHECKBOX_Ninite_Firefox.Add_CheckStateChanged( { Set-NiniteButtonState } )
-
 [Windows.Forms.CheckBox]$CHECKBOX_Ninite_7zip = New-CheckBox '7-Zip' -Name '7zip' -Checked
-$CHECKBOX_Ninite_7zip.Add_CheckStateChanged( { Set-NiniteButtonState } )
-
 [Windows.Forms.CheckBox]$CHECKBOX_Ninite_VLC = New-CheckBox 'VLC' -Name 'vlc' -Checked
-$CHECKBOX_Ninite_VLC.Add_CheckStateChanged( { Set-NiniteButtonState } )
-
 [Windows.Forms.CheckBox]$CHECKBOX_Ninite_AnyDesk = New-CheckBox 'AnyDesk' -Name 'anydesk' -Checked
-$CHECKBOX_Ninite_AnyDesk.Add_CheckStateChanged( { Set-NiniteButtonState } )
-
 [Windows.Forms.CheckBox]$CHECKBOX_Ninite_qBittorrent = New-CheckBox 'qBittorrent' -Name 'qbittorrent'
-$CHECKBOX_Ninite_qBittorrent.Add_CheckStateChanged( { Set-NiniteButtonState } )
-
 [Windows.Forms.CheckBox]$CHECKBOX_Ninite_Malwarebytes = New-CheckBox 'Malwarebytes' -Name 'malwarebytes'
-$CHECKBOX_Ninite_Malwarebytes.Add_CheckStateChanged( { Set-NiniteButtonState } )
 
 
 [ScriptBlock]$BUTTON_FUNCTION = { Get-NiniteInstaller -OpenInBrowser:(-not $CHECKBOX_StartNinite.Enabled) -Execute:$CHECKBOX_StartNinite.Checked }
@@ -46,3 +33,7 @@ Set-Variable -Option Constant NINITE_CHECKBOXES (
         $CHECKBOX_Ninite_Malwarebytes
     )
 )
+
+foreach ($Checkbox in $NINITE_CHECKBOXES) {
+    $Checkbox.Add_CheckStateChanged( { Set-NiniteButtonState } )
+}
