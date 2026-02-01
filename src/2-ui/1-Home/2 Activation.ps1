088626f987e83ba231aa150eda5802afbb6b1032
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2026-02-02 01:36:55 +0200

    Drop support for Windows older than 7

--- src/2-ui/1-Home/2 Activation.ps1
+++ src/2-ui/1-Home/2 Activation.ps1
@@ -1,9 +1,8 @@
 New-GroupBox 'Activation'
 
 
-[Switch]$BUTTON_DISABLED = $OS_VERSION -lt 7
 [ScriptBlock]$BUTTON_FUNCTION = { Start-Activator -ActivateWindows:$CHECKBOX_ActivateWindows.Checked -ActivateOffice:$CHECKBOX_ActivateOffice.Checked }
-New-Button 'MAS Activator' $BUTTON_FUNCTION -Disabled:$BUTTON_DISABLED
+New-Button 'MAS Activator' $BUTTON_FUNCTION
 
 [Windows.Forms.CheckBox]$CHECKBOX_ActivateWindows = New-CheckBox 'Activate Windows'
 
