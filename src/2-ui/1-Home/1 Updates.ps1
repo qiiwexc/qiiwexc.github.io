cf8455badd9076e19dd7577676b4733ad87efe68
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2026-02-05 00:57:23 +0200

    Change some texts

--- src/2-ui/1-Home/1 Updates.ps1
+++ src/2-ui/1-Home/1 Updates.ps1
@@ -2,13 +2,13 @@ New-GroupBox 'Updates'
 
 
 [ScriptBlock]$BUTTON_FUNCTION = { Update-Windows }
-New-Button 'Windows update' $BUTTON_FUNCTION
+New-Button 'Update Windows' $BUTTON_FUNCTION
 
 
 [ScriptBlock]$BUTTON_FUNCTION = { Update-MicrosoftStoreApps }
-New-Button 'Microsoft Store updates' $BUTTON_FUNCTION
+New-Button 'Update Store apps' $BUTTON_FUNCTION
 
 
 [Switch]$BUTTON_DISABLED = $OFFICE_INSTALL_TYPE -ne 'C2R'
 [ScriptBlock]$BUTTON_FUNCTION = { Update-MicrosoftOffice }
-New-Button 'Microsoft Office updates' $BUTTON_FUNCTION -Disabled:$BUTTON_DISABLED
+New-Button 'Update Microsoft Office' $BUTTON_FUNCTION -Disabled:$BUTTON_DISABLED
--- src/2-ui/1-Home/2 Activation.ps1
+++ src/2-ui/1-Home/2 Activation.ps1
@@ -4,6 +4,6 @@ New-GroupBox 'Activation'
 [ScriptBlock]$BUTTON_FUNCTION = { Start-Activator -ActivateWindows:$CHECKBOX_ActivateWindows.Checked -ActivateOffice:$CHECKBOX_ActivateOffice.Checked }
 New-Button 'MAS Activator' $BUTTON_FUNCTION
 
-[Windows.Forms.CheckBox]$CHECKBOX_ActivateWindows = New-CheckBox 'Activate Windows'
+[Windows.Forms.CheckBox]$CHECKBOX_ActivateWindows = New-CheckBox 'Activate Windows silently'
 
-[Windows.Forms.CheckBox]$CHECKBOX_ActivateOffice = New-CheckBox 'Activate Office' -Padded
+[Windows.Forms.CheckBox]$CHECKBOX_ActivateOffice = New-CheckBox 'Activate Office silently' -Padded
