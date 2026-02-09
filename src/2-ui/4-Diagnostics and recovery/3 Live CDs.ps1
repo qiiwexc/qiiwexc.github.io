252c9568a494dae7b096fffa7413a5090ee7d88b
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2026-02-09 22:45:37 +0200

    Add live CD downloads

---
+++ src/2-ui/4-Diagnostics and recovery/3 Live CDs.ps1
@@ -0,0 +1,9 @@
+New-GroupBox 'Live CDs'
+
+
+[ScriptBlock]$BUTTON_FUNCTION = { Open-InBrowser '{URL_WINDOWS_PE}' }
+New-ButtonBrowser 'Download Windows PE' $BUTTON_FUNCTION
+
+
+[ScriptBlock]$BUTTON_FUNCTION = { Open-InBrowser '{URL_SYSTEM_RESCUE}' }
+New-ButtonBrowser 'Download SystemRescue' $BUTTON_FUNCTION
