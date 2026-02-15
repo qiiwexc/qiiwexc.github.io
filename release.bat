57e1f17d53f471d39b9014ccd727e49934b42688
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2026-02-15 17:31:53 +0200

    Minor improvements

--- release.bat
+++ release.bat
@@ -2,7 +2,7 @@
 
 for /f %%i in ('powershell -NoProfile -Command "Get-Date -Format \"y.M.d\""') do set version=%%i
 
-git fetch --all --prune
-git pull --tags --autostash -r origin master
-git tag -a v%version% -m "Release v%version%"
-git push origin --tags
+git fetch --all --prune || (echo Error: git fetch failed & exit /b 1)
+git pull --tags --autostash -r origin master || (echo Error: git pull failed & exit /b 1)
+git tag -a v%version% -m "Release v%version%" || (echo Error: git tag failed & exit /b 1)
+git push origin --tags || (echo Error: git push failed & exit /b 1)
