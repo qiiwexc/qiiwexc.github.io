e2a7ff1a2eb0486c21b643d89a68eafaa1ab1836
qiiwexc <11026937+qiiwexc@users.noreply.github.com>
2026-02-15 17:31:53 +0200

    Minor improvements

--- install-dependencies.bat
+++ install-dependencies.bat
@@ -1,3 +1,6 @@
 @echo off
 
-powershell "Install-Module PSScriptAnalyzer, Pester"
+set PSSCRIPTANALYZER_VERSION=1.24.0
+set PESTER_VERSION=5.7.1
+
+powershell -NoProfile -Command "Set-PSRepository PSGallery -InstallationPolicy Trusted; Install-Module -Name PSScriptAnalyzer -Force -Scope CurrentUser -MinimumVersion %PSSCRIPTANALYZER_VERSION%; Install-Module -Name Pester -Force -Scope CurrentUser -MinimumVersion %PESTER_VERSION%"
